80b74f3a146ef6203ffdf65ed7c3811342f9e420
Caleb Brinkman <floralvikings@gmail.com>
2013-09-18 22:02:57 -0500

    Implemented ExecutableObjectVisible message.

--- jgcf/src/main/java/com/jenjinstudios/jgcf/message/ExecutableObjectVisibleMessage.java
+++ jgcf/src/main/java/com/jenjinstudios/jgcf/message/ExecutableObjectVisibleMessage.java
@@ -2,6 +2,8 @@ package com.jenjinstudios.jgcf.message;
 
 import com.jenjinstudios.jgcf.WorldClient;
 import com.jenjinstudios.message.Message;
+import com.jenjinstudios.world.ClientActor;
+import com.jenjinstudios.world.ClientObject;
 
 /**
  * Process an ActorVisibleMessage.
@@ -10,6 +12,8 @@ import com.jenjinstudios.message.Message;
  */
 public class ExecutableObjectVisibleMessage extends WorldClientExecutableMessage
 {
+	/** The newly visible actor. */
+	ClientObject newlyVisible;
 
 	/**
 	 * Construct an ExecutableMessage with the given Message.
@@ -25,10 +29,19 @@ public class ExecutableObjectVisibleMessage extends WorldClientExecutableMessage
 	@Override
 	public void runSynced()
 	{
+		getClient().addNewVisible(newlyVisible);
 	}
 
 	@Override
 	public void runASync()
 	{
+		Message message = getMessage();
+		String name = (String) message.getArgument("name");
+		int id = (int) message.getArgument("id");
+		double xCoord = (double) message.getArgument("xCoordinate");
+		double zCoord = (double) message.getArgument("zCoordinate");
+
+		newlyVisible = new ClientActor(id, name);
+		newlyVisible.setVector2D(xCoord, zCoord);
 	}
 }
