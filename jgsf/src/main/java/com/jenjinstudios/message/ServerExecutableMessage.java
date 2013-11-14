2945c0e721d85d2dd7e15d1eeb1387f04576eb8d
Caleb Brinkman <floralvikings@gmail.com>
2013-11-13 19:41:08 -0600

    Error is handled by sending an "InvalidMessage" message to the client.

--- jgsf/src/main/java/com/jenjinstudios/message/ServerExecutableMessage.java
+++ jgsf/src/main/java/com/jenjinstudios/message/ServerExecutableMessage.java
@@ -43,7 +43,7 @@ public abstract class ServerExecutableMessage extends ExecutableMessage
 		MessageType messageType = MessageRegistry.getMessageType(message.getID());
 		// Get the executable message classes registered.
 		Class<? extends ExecutableMessage> execClass = messageType.serverExecutableMessageClass;
-		try // TODO Make sure error is handled gracefully
+		try
 		{
 			// Get and parse the Constructors for the ExecutableMessage class retrieved.
 			Constructor<? extends ExecutableMessage>[] execConstructors;
