c0f4dcdbb5b1baa8fa16b02078b85e31602f9b91
Caleb Brinkman <floralvikings@gmail.com>
2013-11-14 00:47:56 -0600

    Error is already handled with boolean return.

--- jgcf/src/main/java/com/jenjinstudios/net/AuthClient.java
+++ jgcf/src/main/java/com/jenjinstudios/net/AuthClient.java
@@ -54,7 +54,7 @@ public class AuthClient extends Client
 		long timepast = System.currentTimeMillis() - startTime;
 		while (!hasReceivedLoginResponse() && (timepast < TIMEOUT_MILLIS))
 		{
-			try // TODO Make sure error is handled gracefully
+			try
 			{
 				Thread.sleep(10);
 			} catch (InterruptedException e)
