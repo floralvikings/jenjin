00557986c763d293e5f8c9b72e53642eeabc8ad5
Caleb Brinkman <floralvikings@gmail.com>
2014-01-26 19:31:11 -0600

    Made properties private, improved thread safety, added method to access properties directly for reading and writing purposes.

--- jenjin-world-utils/src/main/java/com/jenjinstudios/world/LocationProperties.java
+++ jenjin-world-utils/src/main/java/com/jenjinstudios/world/LocationProperties.java
@@ -9,23 +9,16 @@ import java.util.TreeMap;
 public class LocationProperties
 {
 	/** The properties stored by this object. */
-	public final TreeMap<String, String> properties;
+	private final TreeMap<String, String> properties;
 
-	/**
-	 * Construct a new LocationProperties with default attributes.
-	 */
-	public LocationProperties()
-	{
-		this(new TreeMap<String, String>());
-	}
+	/** Construct a new LocationProperties with default attributes. */
+	public LocationProperties() { this(new TreeMap<String, String>()); }
 
 	/**
 	 * Construct a new Property.
 	 * @param properties The properties to be stored by this object.
 	 */
-	public LocationProperties(TreeMap<String, String> properties) {
-		this.properties = new TreeMap<>(properties);
-	}
+	public LocationProperties(TreeMap<String, String> properties) { this.properties = new TreeMap<>(properties); }
 
 	/**
 	 * Get the property with the given name.
@@ -33,6 +26,15 @@ public class LocationProperties
 	 * @return The value of the property.
 	 */
 	public String getProperty(String name) {
-		return properties.get(name);
+		synchronized (properties)
+		{
+			return properties.get(name);
+		}
 	}
+
+	/**
+	 * Get the map used to store properties; usage of this method is <b>not</b> recommended for thread-safety purposes.
+	 * @return A copy of the map of properties.
+	 */
+	public TreeMap<String, String> getProperties() { return properties; }
 }
