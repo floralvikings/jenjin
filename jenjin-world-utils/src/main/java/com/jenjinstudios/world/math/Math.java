11cda0ddf444acc6007861786b501879b031498b
Caleb Brinkman <floralvikings@gmail.com>
2014-01-27 22:46:06 -0600

    Renamed Round class to Math

--- jenjin-world-utils/src/main/java/com/jenjinstudios/world/math/Math.java
+++ jenjin-world-utils/src/main/java/com/jenjinstudios/world/math/Math.java
@@ -4,10 +4,10 @@ package com.jenjinstudios.world.math;
  * This class is used to simplify rounding.
  * @author Caleb Brinkman
  */
-public class Round
+public class Math
 {
 	/**
-	 * Round the given number to the given decimal place.
+	 * Math the given number to the given decimal place.
 	 * @param value The number to round.
 	 * @param decimalPlaces The number of decimals to be rounded to.
 	 * @return The rounded value.
@@ -15,9 +15,9 @@ public class Round
 	public static double round(double value, int decimalPlaces) {
 		if (decimalPlaces < 0) throw new IllegalArgumentException();
 
-		long factor = (long) Math.pow(10, decimalPlaces);
+		long factor = (long) java.lang.Math.pow(10, decimalPlaces);
 		value = value * factor;
-		long tmp = Math.round(value);
+		long tmp = java.lang.Math.round(value);
 		return (double) tmp / factor;
 	}
 }
