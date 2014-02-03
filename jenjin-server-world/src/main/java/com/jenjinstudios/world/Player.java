605f237a3e60494bc0f0d8be55e3e8cef68cd21d
Caleb Brinkman <floralvikings@gmail.com>
2014-02-02 19:03:25 -0600

    Added constructors matching superclass.

--- jenjin-server-world/src/main/java/com/jenjinstudios/world/Player.java
+++ jenjin-server-world/src/main/java/com/jenjinstudios/world/Player.java
@@ -6,4 +6,20 @@ package com.jenjinstudios.world;
  */
 public class Player extends Actor
 {
+	/**
+	 * Construct a player with the given username.
+	 * @param username The username.
+	 */
+	public Player(String username) {
+		super(username);
+	}
+
+	/**
+	 * Construct a player with the given username and id.
+	 * @param username The username.
+	 * @param id The id.
+	 */
+	public Player(String username, int id) {
+		super(username, id);
+	}
 }
