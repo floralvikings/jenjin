8ad2b7ed12e974e41ff4b8841695ce3e59945e65
Caleb Brinkman <floralvikings@gmail.com>
2014-02-11 17:54:53 -0600

    Improved async method.

--- jenjin-server-world/src/main/java/com/jenjinstudios/world/message/ExecutableStateChangeRequest.java
+++ jenjin-server-world/src/main/java/com/jenjinstudios/world/message/ExecutableStateChangeRequest.java
@@ -5,12 +5,20 @@ import com.jenjinstudios.world.Actor;
 import com.jenjinstudios.world.WorldClientHandler;
 import com.jenjinstudios.world.state.MoveState;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Process a StateChangeRequest.
  * @author Caleb Brinkman
  */
 public class ExecutableStateChangeRequest extends WorldExecutableMessage
 {
+	/** The Logger for this class. */
+	private static final Logger LOGGER = Logger.getLogger(ExecutableStateChangeRequest.class.getName());
+	/** The state being requested. */
+	private MoveState newState;
+
 	/**
 	 * Construct a new ExecutableMessage.  Must be implemented by subclasses.
 	 * @param handler The handler using this ExecutableMessage.
@@ -23,15 +31,15 @@ public class ExecutableStateChangeRequest extends WorldExecutableMessage
 	@Override
 	public void runSynced() {
 		Actor player = getClientHandler().getPlayer();
-		MoveState newState;
-		double direction = (double) getMessage().getArgument("relativeAngle");
-		double angle = (double) getMessage().getArgument("absoluteAngle");
-		int stepsFromLast = (int) getMessage().getArgument("stepsUntilChange");
-		newState = new MoveState(direction, stepsFromLast, angle);
 		player.addMoveState(newState);
 	}
 
 	@Override
 	public void runASync() {
+		double direction = (double) getMessage().getArgument("relativeAngle");
+		double angle = (double) getMessage().getArgument("absoluteAngle");
+		int stepsFromLast = (int) getMessage().getArgument("stepsUntilChange");
+		newState = new MoveState(direction, stepsFromLast, angle);
+		LOGGER.log(Level.FINEST, "Got State Change Request: " + newState);
 	}
 }
