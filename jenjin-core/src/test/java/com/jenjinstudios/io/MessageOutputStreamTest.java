07af0a6afe708a73ada9295c6854d8e94e842a1e
Caleb Brinkman <Caleb.Brinkman@whitehatsec.com>
2014-06-14 15:28:44 -0500

    Improving test coverage of core classes

--- jenjin-core/src/test/java/com/jenjinstudios/io/MessageOutputStreamTest.java
+++ jenjin-core/src/test/java/com/jenjinstudios/io/MessageOutputStreamTest.java
@@ -26,20 +26,12 @@ public class MessageOutputStreamTest
 
 		mos.writeMessage(msg);
 		byte[] bytes = bos.toByteArray();
-
 		mos.close();
 
-		// Have to read the data back out in order.
-		DataInputStream dis = new DataInputStream(new ByteArrayInputStream(bytes));
-		short id = dis.readShort();
-		boolean encrypted = dis.readBoolean();
-		String msgName = dis.readUTF();
-		short invID = dis.readShort();
-
-		Assert.assertEquals(id, msg.getID());
-		Assert.assertEquals(invID, msg.getArgument("messageID"));
-		Assert.assertEquals(encrypted, false);
-		Assert.assertEquals(msgName, msg.getArgument("messageName"));
+		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+		MessageInputStream mis = new MessageInputStream(mr, bis);
+		Message readMsg = mis.readMessage();
+		Assert.assertEquals(readMsg.getArgs(), msg.getArgs());
 	}
 
 	@Test(expectedExceptions = {IOException.class})
@@ -80,5 +72,33 @@ public class MessageOutputStreamTest
 		Assert.assertEquals(id, msg.getID());
 		Assert.assertTrue(encrypted);
 		Assert.assertNotEquals(encStr, msg.getArgument("encryptedString"));
+	}
+
+	@Test
+	public void testAllTypesMessage() throws Exception {
+		MessageRegistry mr = new MessageRegistry(false);
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		MessageOutputStream mos = new MessageOutputStream(mr, bos);
+
+		Message msg = mr.createMessage("TestAllTypesMessage");
+		msg.setArgument("testString", "SNAFU");
+		msg.setArgument("testInt", 123);
+		msg.setArgument("testLong", 456l);
+		msg.setArgument("testDouble", 4.567);
+		msg.setArgument("testFloat", 0.123f);
+		msg.setArgument("testShort", (short) 789);
+		msg.setArgument("testBoolean", true);
+		msg.setArgument("testByte", (byte) 101);
+		msg.setArgument("testByteArray", new byte[]{2, 3, 5, 7, 11});
+		msg.setArgument("testStringArray", new String[]{"Foo", "Bar"});
+
+		mos.writeMessage(msg);
+		byte[] bytes = bos.toByteArray();
+		mos.close();
+
+		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+		MessageInputStream mis = new MessageInputStream(mr, bis);
+		Message readMsg = mis.readMessage();
+		Assert.assertEquals(readMsg.getArgs(), msg.getArgs());
 	}
 }
