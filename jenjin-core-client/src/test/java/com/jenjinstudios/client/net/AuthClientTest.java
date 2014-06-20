7a3c0ebf7c4b3afb69f1773617ff0c4923be22f0
Caleb Brinkman <Caleb.Brinkman@whitehatsec.com>
2014-06-19 20:01:29 -0500

    Code formatting

---
+++ jenjin-core-client/src/test/java/com/jenjinstudios/client/net/AuthClientTest.java
@@ -0,0 +1,17 @@
+package com.jenjinstudios.client.net;
+
+import org.testng.annotations.Test;
+
+public class AuthClientTest
+{
+
+	@Test
+	public void testSendBlockingLoginRequest() throws Exception {
+
+	}
+
+	@Test
+	public void testSendBlockingLogoutRequest() throws Exception {
+
+	}
+}
