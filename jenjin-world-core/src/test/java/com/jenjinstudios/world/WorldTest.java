ecfb815ef8c76d7b1a317fe7834e857661b9ee0d
Caleb Brinkman <Caleb.Brinkman@whitehatsec.com>
2014-06-29 01:37:44 -0500

    Added testAddObject method.

--- jenjin-world-core/src/test/java/com/jenjinstudios/world/WorldTest.java
+++ jenjin-world-core/src/test/java/com/jenjinstudios/world/WorldTest.java
@@ -1,8 +1,25 @@
 package com.jenjinstudios.world;
 
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 /**
  * @author Caleb Brinkman
  */
 public class WorldTest
 {
+	@Test
+	public void testAddObject() {
+		WorldObject obj0 = Mockito.mock(WorldObject.class);
+		WorldObject obj1 = Mockito.mock(WorldObject.class);
+		WorldObject obj2 = new WorldObject("Bob");
+		World world = new World();
+
+		world.addObject(obj0);
+		world.addObject(obj1);
+		world.addObject(obj2);
+
+		Assert.assertEquals(obj2.getId(), 2);
+	}
 }
