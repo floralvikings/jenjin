d7c9a35a1fe32dcb0926289b58173a7ffcc4518d
Caleb Brinkman <Caleb.Brinkman@whitehatsec.com>
2014-07-02 20:20:04 -0500

    Refactoring, removed old visibility methods

--- jenjin-world-core/src/main/java/com/jenjinstudios/world/Zone.java
+++ jenjin-world-core/src/main/java/com/jenjinstudios/world/Zone.java
@@ -1,15 +1,7 @@
 package com.jenjinstudios.world;
 
 import com.jenjinstudios.world.math.Dimension2D;
-import com.jenjinstudios.world.math.MathUtil;
 import com.jenjinstudios.world.math.Vector2D;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * The {@code Zone} class represents a grid of {@code Location} objects within the {@code World}.  Zones cannot be
@@ -18,8 +10,6 @@ import java.util.logging.Logger;
  */
 public class Zone
 {
-	/** The Logger for this class. */
-	private static final Logger LOGGER = Logger.getLogger(Zone.class.getName());
 	/** The number assigned to this Zone by the world on initialization. */
 	public final int id;
 	/** The number of {@code Location} objects in the x-axis. */
@@ -40,11 +30,8 @@ public class Zone
 		this.ySize = size.getYSize();
 
 		locationGrid = new Location[size.getXSize()][size.getYSize()];
-		LOGGER.log(Level.FINEST, "Constructing Locations.");
 		constructLocations();
-		LOGGER.log(Level.FINEST, "Adding Special Locations.");
 		addSpecialLocations(specialLocations);
-		LOGGER.log(Level.FINEST, "Calculating Location Adjacency.");
 		initializeLocationAdjacency();
 	}
 
@@ -89,157 +76,6 @@ public class Zone
 		return loc;
 	}
 
-	/**
-	 * Cast a circle using the given center and radius, returning all locations within the circle.  This method is very
-	 * slow and memory-intensive, so it should not be used during world updates.  Instead, call this method during
-	 * initialization.
-	 * @param center The center location.
-	 * @param radius The radius of the circle.
-	 * @return All locations lying within the specified circle.
-	 */
-	@SuppressWarnings("SuspiciousNameCombination")
-	public Collection<Location> castVisibilityCircle(Location center, int radius) {
-		LinkedList<Location> locations = new LinkedList<>();
-		int centerY = center.X_COORDINATE;
-		int centerX = center.Y_COORDINATE;
-		HashSet<Location> visibleLocations = new HashSet<>();
-
-		List<Vector2D> circle = MathUtil.castCircle(centerX, centerY, radius);
-
-		for (Vector2D vector2D : circle)
-		{
-			int x = (int) vector2D.getXCoordinate();
-			int y = (int) vector2D.getYCoordinate();
-			visibleLocations.addAll(castVisibilityRay(centerX, centerY, x, y));
-		}
-
-		locations.addAll(visibleLocations);
-		return locations;
-	}
-
-	/**
-	 * This uses a modified version of Bresenham's Line Algorithm, available in its original form <a
-	 * href=http://lifc.univ-fcomte.fr/~dedu/projects/bresenham/index.html>here.</a>  This algorithm works by casting a
-	 * ray until a Location with a LocationProperty containing the property "blocksVision" set to "true".
-	 * @param x1 The starting x location.
-	 * @param y1 The starting y location.
-	 * @param x2 The ending x location.
-	 * @param y2 The ending y location.
-	 * @return The ray cast from the given starting points to the given end points.
-	 */
-	@SuppressWarnings("SuspiciousNameCombination")
-	protected Collection<Location> castVisibilityRay(int x1, int y1, int x2, int y2) {
-		// FIXME This algorithm is ugly, complex, and breaks lots of clean code practices.  Needs to be fixed.
-		LinkedList<Location> visibleRay = new LinkedList<>();
-		int i;               // loop counter
-		int yStep, xStep;    // the step on y and x axis
-		int error;           // the error accumulated during the increment
-		int previousError;       // *vision the previous value of the error variable
-		int y = y1, x = x1;  // the line points
-		double ddy, ddx;        // compulsory variables: the double values of dy and dx
-		int dx = x2 - x1;
-		int dy = y2 - y1;
-		visibleRay.add(getLocationOnGrid(y1, x1));  // first point
-		// NB the last point can't be here, because of its previous point (which has to be verified)
-		if (dy < 0)
-		{
-			yStep = -1;
-			dy = -dy;
-		} else
-		{
-			yStep = 1;
-		}
-		if (dx < 0)
-		{
-			xStep = -1;
-			dx = -dx;
-		} else
-		{
-			xStep = 1;
-		}
-		ddy = 2 * dy;  // work with double values for full precision
-		ddx = 2 * dx;
-		if (ddx >= ddy)
-		{  // first octant (0 <= slope <= 1)
-			// compulsory initialization (even for previousError, needed when dx==dy)
-			previousError = error = dx;  // start in the middle of the square
-			for (i = 0; i < dx; i++)
-			{  // do not use the first point (already done)
-				x += xStep;
-				error += ddy;
-				if (error > ddx)
-				{  // increment y if AFTER the middle ( > )
-					y += yStep;
-					error -= ddx;
-					// three cases (octant == right->right-top for directions below):
-					if (error + previousError < ddx)  // bottom square also
-					{
-						if (!addLocationToVisibilityRay(y - yStep, x, visibleRay)) break;
-					} else if (error + previousError > ddx)  // left square also
-					{
-						if (!addLocationToVisibilityRay(y, x - xStep, visibleRay)) break;
-					} else
-					{  // corner: bottom and left squares also
-						if (!addLocationToVisibilityRay(y - yStep, x, visibleRay)) break;
-						if (!addLocationToVisibilityRay(y, x - xStep, visibleRay)) break;
-					}
-				}
-				if (!addLocationToVisibilityRay(y, x, visibleRay)) break;
-				previousError = error;
-			}
-		} else
-		{  // the same as above
-			previousError = error = dy;
-			for (i = 0; i < dy; i++)
-			{
-				y += yStep;
-				error += ddx;
-				if (error > ddy)
-				{
-					x += xStep;
-					error -= ddy;
-					if (error + previousError < ddy)
-					{
-						if (!addLocationToVisibilityRay(y, x - xStep, visibleRay)) break;
-					} else
-					{
-						if (error + previousError > ddy)
-						{
-							if (!addLocationToVisibilityRay(y - yStep, x, visibleRay)) break;
-						} else
-						{
-							if (!addLocationToVisibilityRay(y, x - xStep, visibleRay)) break;
-							if (!addLocationToVisibilityRay(y - yStep, x, visibleRay)) break;
-						}
-					}
-				}
-				if (!addLocationToVisibilityRay(y, x, visibleRay)) break;
-				previousError = error;
-			}
-		}
-		return visibleRay;
-	}
-
-	/**
-	 * Add the location at the given coordinates to the specified ray, returning true if the location was added, false
-	 * if not.
-	 * @param x The x coordinate.
-	 * @param y The y coordinate.
-	 * @param ray The ray.
-	 * @return true if the location was added.
-	 */
-	@SuppressWarnings("BooleanMethodIsAlwaysInverted")
-	private boolean addLocationToVisibilityRay(int x, int y, List<Location> ray) {
-		Location location = getLocationOnGrid(x, y);
-		if (location == null || "true".equals(location.getProperties().getProperty("blocksVision")))
-		{
-			return false;
-		}
-		ray.add(location);
-		return true;
-	}
-
-	/** Initialize the locations in the zone. */
 	private void constructLocations() {
 		for (int x = 0; x < xSize; x++)
 			constructColumn(x);
