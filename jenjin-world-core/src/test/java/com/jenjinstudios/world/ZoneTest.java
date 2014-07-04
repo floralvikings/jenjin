c7bca9640b85f1150a8da445dfc9af2d16b5fe0a
Caleb Brinkman <Caleb.Brinkman@whitehatsec.com>
2014-07-04 02:03:16 -0500

    Added test for zone constructor

--- jenjin-world-core/src/test/java/com/jenjinstudios/world/ZoneTest.java
+++ jenjin-world-core/src/test/java/com/jenjinstudios/world/ZoneTest.java
@@ -1,6 +1,10 @@
 package com.jenjinstudios.world;
 
+import com.jenjinstudios.world.math.Dimension2D;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.Properties;
 
 /**
  * @author Caleb Brinkman
@@ -9,6 +13,13 @@ public class ZoneTest
 {
 	@Test
 	public void testConstructor() {
+		Properties properties = new Properties();
+		properties.setProperty("blocksVision", "true");
+		Location specialLocation = new Location(10, 10, properties);
 
+		Zone zone = new Zone(0, new Dimension2D(20, 20), specialLocation);
+
+		Location actualLoc = zone.getLocationOnGrid(10, 10);
+		Assert.assertEquals(actualLoc, specialLocation);
 	}
 }
