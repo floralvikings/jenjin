2cd388be0e0174e87b497323a6011b4390d10eef
Caleb Brinkman <floralvikings@gmail.com>
2014-07-10 00:15:46 -0500

    Added ExecutableWorldLoginRequestTest

---
+++ jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldLoginRequestTest.java
@@ -0,0 +1,78 @@
+package com.jenjinstudios.world.server.message;
+
+import com.jenjinstudios.core.io.Message;
+import com.jenjinstudios.core.io.MessageRegistry;
+import com.jenjinstudios.server.net.User;
+import com.jenjinstudios.world.World;
+import com.jenjinstudios.world.math.Vector2D;
+import com.jenjinstudios.world.server.Player;
+import com.jenjinstudios.world.server.WorldClientHandler;
+import com.jenjinstudios.world.server.WorldServer;
+import com.jenjinstudios.world.server.sql.WorldAuthenticator;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Caleb Brinkman
+ */
+public class ExecutableWorldLoginRequestTest
+{
+	@Test
+	public void testSuccessfulLogin() {
+		MessageRegistry messageRegistry = new MessageRegistry();
+		Message loginRequest = messageRegistry.createMessage("WorldLoginRequest");
+		loginRequest.setArgument("username", "foo");
+		loginRequest.setArgument("password", "bar");
+		Message loginResponse = messageRegistry.createMessage("WorldLoginResponse");
+
+		World world = mock(World.class);
+		Player player = mock(Player.class);
+		WorldServerMessageFactory messageFactory = mock(WorldServerMessageFactory.class);
+		WorldClientHandler handler = mock(WorldClientHandler.class);
+		WorldServer worldServer = mock(WorldServer.class);
+		WorldAuthenticator authenticator = mock(WorldAuthenticator.class);
+		when(messageFactory.generateWorldLoginResponse()).thenReturn(loginResponse);
+		when(worldServer.getAuthenticator()).thenReturn(authenticator);
+		when(worldServer.getWorld()).thenReturn(world);
+		when(handler.getServer()).thenReturn(worldServer);
+		when(handler.getMessageFactory()).thenReturn(messageFactory);
+		when(authenticator.logInPlayer(any(User.class))).thenReturn(player);
+		when(player.getId()).thenReturn(0);
+		when(player.getVector2D()).thenReturn(Vector2D.ORIGIN);
+
+		ExecutableWorldLoginRequest exec = new ExecutableWorldLoginRequest(handler, loginRequest);
+		exec.runImmediate();
+		exec.runDelayed();
+
+		verify(world).addObject(player);
+		verify(handler).queueOutgoingMessage(loginResponse);
+	}
+
+	@Test
+	public void testFailedLogin() {
+		MessageRegistry messageRegistry = new MessageRegistry();
+		Message loginRequest = messageRegistry.createMessage("WorldLoginRequest");
+		loginRequest.setArgument("username", "foo");
+		loginRequest.setArgument("password", "bar");
+		Message loginResponse = messageRegistry.createMessage("WorldLoginResponse");
+
+		World world = mock(World.class);
+		WorldServerMessageFactory messageFactory = mock(WorldServerMessageFactory.class);
+		WorldClientHandler handler = mock(WorldClientHandler.class);
+		WorldServer worldServer = mock(WorldServer.class);
+		WorldAuthenticator authenticator = mock(WorldAuthenticator.class);
+		when(messageFactory.generateWorldLoginResponse()).thenReturn(loginResponse);
+		when(worldServer.getAuthenticator()).thenReturn(authenticator);
+		when(worldServer.getWorld()).thenReturn(world);
+		when(handler.getServer()).thenReturn(worldServer);
+		when(handler.getMessageFactory()).thenReturn(messageFactory);
+		when(authenticator.logInPlayer(any(User.class))).thenReturn(null);
+
+		ExecutableWorldLoginRequest exec = new ExecutableWorldLoginRequest(handler, loginRequest);
+		exec.runImmediate();
+		exec.runDelayed();
+
+		verify(handler).queueOutgoingMessage(loginResponse);
+	}
+}
