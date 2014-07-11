a6ee54d82418dd47ef2c398de4ffb4c85eb9eda1
Caleb Brinkman <floralvikings@gmail.com>
2014-07-10 19:48:10 -0500

    Refactored to reduce wonkiness

--- jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableStateChangeMessageTest.java
+++ jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableStateChangeMessageTest.java
@@ -1,39 +1,44 @@
 package com.jenjinstudios.world.client.message;
 
 import com.jenjinstudios.core.io.Message;
+import com.jenjinstudios.core.io.MessageRegistry;
+import com.jenjinstudios.world.World;
 import com.jenjinstudios.world.client.ClientActor;
+import com.jenjinstudios.world.client.WorldClient;
 import com.jenjinstudios.world.math.Angle;
-import com.jenjinstudios.world.math.Vector2D;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import static com.jenjinstudios.world.math.Angle.*;
+import static java.lang.Math.*;
+import static org.mockito.Mockito.*;
 
 /**
  * @author Caleb Brinkman
  */
-public class ExecutableStateChangeMessageTest extends WorldClientExecutableMessageTest
+public class ExecutableStateChangeMessageTest
 {
-	@Test(timeOut = 5000)
-	@Override
+	@Test
 	public void testMessageExecution() throws Exception {
-		ClientActor clientActor = new ClientActor(100, "Bob");
-
-		worldClient.blockingStart();
-		worldClient.sendBlockingWorldFileRequest();
-		worldClient.sendBlockingLoginRequest();
-		worldClient.getWorld().addObject(clientActor, 100);
-
+		MessageRegistry messageRegistry = new MessageRegistry();
 		Message stateChangeMessage = messageRegistry.createMessage("StateChangeMessage");
 		stateChangeMessage.setArgument("id", 100);
-		stateChangeMessage.setArgument("relativeAngle", Angle.IDLE);
-		stateChangeMessage.setArgument("absoluteAngle", Math.PI);
+		stateChangeMessage.setArgument("relativeAngle", IDLE);
+		stateChangeMessage.setArgument("absoluteAngle", PI);
 		stateChangeMessage.setArgument("timeOfChange", System.nanoTime());
-		stateChangeMessage.setArgument("xCoordinate", Math.PI);
-		stateChangeMessage.setArgument("yCoordinate", Math.PI);
-		inStreamReadMessage.thenReturn(stateChangeMessage, blankMessageSpam);
-		Thread.sleep(500); // Sleep to allow client to "catch up"
+		stateChangeMessage.setArgument("xCoordinate", PI);
+		stateChangeMessage.setArgument("yCoordinate", PI);
 
-		assertEquals(clientActor.getVector2D(), new Vector2D(Math.PI, Math.PI));
-		assertEquals(clientActor.getAngle(), new Angle(Math.PI, Angle.IDLE));
+		WorldClient worldClient = mock(WorldClient.class);
+		World world = mock(World.class);
+		ClientActor clientActor = mock(ClientActor.class);
+		when(worldClient.getWorld()).thenReturn(world);
+		when(world.getObject(100)).thenReturn(clientActor);
+
+		ExecutableStateChangeMessage message = new ExecutableStateChangeMessage(worldClient, stateChangeMessage);
+		message.runImmediate();
+		message.runDelayed();
+
+		verify(world).getObject(100);
+		verify(clientActor).setAngle(eq(new Angle(PI)));
 	}
 }
