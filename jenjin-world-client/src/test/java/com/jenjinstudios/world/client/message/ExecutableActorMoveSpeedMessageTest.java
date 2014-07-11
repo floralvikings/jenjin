55cc68cf3d9ab0472a1860920e159a5f7b947838
Caleb Brinkman <floralvikings@gmail.com>
2014-07-10 19:15:39 -0500

    Refactored to make better.  Words.

--- jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableActorMoveSpeedMessageTest.java
+++ jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableActorMoveSpeedMessageTest.java
@@ -1,26 +1,29 @@
 package com.jenjinstudios.world.client.message;
 
 import com.jenjinstudios.core.io.Message;
+import com.jenjinstudios.core.io.MessageRegistry;
 import com.jenjinstudios.world.client.ClientActor;
+import com.jenjinstudios.world.client.WorldClient;
+import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
  * @author Caleb Brinkman
  */
-public class ExecutableActorMoveSpeedMessageTest extends WorldClientExecutableMessageTest
+public class ExecutableActorMoveSpeedMessageTest
 {
-	@Test(timeOut = 5000)
-	@Override
+	@Test
 	public void testMessageExecution() throws Exception {
+		MessageRegistry messageRegistry = new MessageRegistry();
 		Message actorMoveSpeedMessage = messageRegistry.createMessage("ActorMoveSpeed");
 		actorMoveSpeedMessage.setArgument("moveSpeed", 123.45);
-		inStreamReadMessage.thenReturn(actorMoveSpeedMessage, blankMessageSpam);
+		WorldClient worldClient = Mockito.mock(WorldClient.class);
 
-		worldClient.blockingStart();
-		worldClient.sendBlockingWorldFileRequest();
-		worldClient.sendBlockingLoginRequest();
-		Thread.sleep(500); // Sleep to allow client to "catch up"
+		ExecutableActorMoveSpeedMessage message =
+			  new ExecutableActorMoveSpeedMessage(worldClient, actorMoveSpeedMessage);
+		message.runImmediate();
+		message.runDelayed();
 
 		Assert.assertEquals(ClientActor.MOVE_SPEED, 123.45);
 	}
