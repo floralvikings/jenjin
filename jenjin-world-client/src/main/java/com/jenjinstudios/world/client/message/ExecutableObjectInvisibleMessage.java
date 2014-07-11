fed6dbdc06e29411569738c86d73453af3d602ba
Caleb Brinkman <floralvikings@gmail.com>
2014-07-10 19:29:18 -0500

    Refactored away pointlessness.

--- jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableObjectInvisibleMessage.java
+++ jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableObjectInvisibleMessage.java
@@ -3,9 +3,6 @@ package com.jenjinstudios.world.client.message;
 import com.jenjinstudios.core.io.Message;
 import com.jenjinstudios.world.client.WorldClient;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  * Handles processing an ActorInvisibleMessage.
  * @author Caleb Brinkman
@@ -13,19 +10,12 @@ import java.util.logging.Logger;
 @SuppressWarnings("WeakerAccess")
 public class ExecutableObjectInvisibleMessage extends WorldClientExecutableMessage
 {
-	private static final Logger LOGGER = Logger.getLogger(ExecutableObjectInvisibleMessage.class.getName());
 	private int id;
 
 	public ExecutableObjectInvisibleMessage(WorldClient client, Message message) { super(client, message); }
 
 	@Override
-	public void runDelayed() {
-		LOGGER.log(Level.FINEST, "Before processing ObjectInvisibleMessage, world contains: {0}",
-			  getClient().getWorld().getObjectCount());
-		getClient().getWorld().removeObject(id);
-		LOGGER.log(Level.FINEST, "After processing ObjectInvisibleMessage, world contains: {0}",
-			  getClient().getWorld().getObjectCount());
-	}
+	public void runDelayed() { getClient().getWorld().removeObject(id); }
 
 	@Override
 	public void runImmediate() { id = (int) getMessage().getArgument("id"); }
