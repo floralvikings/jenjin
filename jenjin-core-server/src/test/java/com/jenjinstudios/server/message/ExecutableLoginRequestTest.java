719618d6fabbd6a12944aae8647e6cb1bdd82c03
Caleb Brinkman <floralvikings@gmail.com>
2014-07-12 05:03:32 -0500

    Test improvementification

--- jenjin-core-server/src/test/java/com/jenjinstudios/server/message/ExecutableLoginRequestTest.java
+++ jenjin-core-server/src/test/java/com/jenjinstudios/server/message/ExecutableLoginRequestTest.java
@@ -6,6 +6,7 @@ import com.jenjinstudios.server.net.AuthServer;
 import com.jenjinstudios.server.net.ClientHandler;
 import com.jenjinstudios.server.net.User;
 import com.jenjinstudios.server.sql.Authenticator;
+import com.jenjinstudios.server.sql.LoginException;
 import org.testng.annotations.Test;
 
 import static org.mockito.Mockito.*;
@@ -42,5 +43,35 @@ public class ExecutableLoginRequestTest
 		executableLoginRequest.runDelayed();
 
 		verify(server).associateUsernameWithClientHandler("foo", clientHandler);
+	}
+
+	@Test
+	public void testFailedLogin() throws Exception {
+		Message message = MessageRegistry.getInstance().createMessage("LoginRequest");
+		message.setArgument("username", "foo-dapoo");
+		message.setArgument("password", "bar");
+
+		User user = new User();
+		user.setUsername("foo");
+		user.setUsername("bar");
+		user.setLoggedIn(true);
+		ClientHandler clientHandler = mock(ClientHandler.class);
+		AuthServer server = mock(AuthServer.class);
+		ServerMessageFactory serverMessageFactory = new ServerMessageFactory(clientHandler,
+			  MessageRegistry.getInstance());
+		Authenticator authenticator = mock(Authenticator.class);
+		when(server.getAuthenticator()).thenReturn(authenticator);
+		when(server.getCycleStartTime()).thenReturn(12345l);
+		when(authenticator.logInUser("foo", "bar")).thenReturn(user);
+		when(authenticator.logInUser("foo-dapoo", "bar")).thenThrow(new LoginException("Nope"));
+		when(clientHandler.getServer()).thenReturn(server);
+		when(clientHandler.getMessageRegistry()).thenReturn(MessageRegistry.getInstance());
+		when(clientHandler.getMessageFactory()).thenReturn(serverMessageFactory);
+
+		ExecutableLoginRequest executableLoginRequest = new ExecutableLoginRequest(clientHandler, message);
+		executableLoginRequest.runImmediate();
+		executableLoginRequest.runDelayed();
+
+		verify(server, times(0)).associateUsernameWithClientHandler("foo-dapoo", clientHandler);
 	}
 }
