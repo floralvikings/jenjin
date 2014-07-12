46ae8eb380d3c3ad136445c816a205e434051192
Caleb Brinkman <floralvikings@gmail.com>
2014-07-12 15:55:51 -0500

    Added AuthClientTest back with much simpler tests

---
+++ jenjin-core-client/src/test/java/com/jenjinstudios/client/AuthClientTest.java
@@ -0,0 +1,46 @@
+package com.jenjinstudios.client;
+
+import com.jenjinstudios.client.net.AuthClient;
+import com.jenjinstudios.client.net.ClientUser;
+import com.jenjinstudios.core.MessageIO;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.mock;
+
+/**
+ * @author Caleb Brinkman
+ */
+public class AuthClientTest
+{
+	@Test
+	public void testIsLoggedIn() {
+		MessageIO messageIO = mock(MessageIO.class);
+		ClientUser clientUser = mock(ClientUser.class);
+		boolean random = Math.random() * 10 % 2 == 0;
+		AuthClient authClient = new AuthClient(messageIO, clientUser);
+		authClient.setLoggedIn(random);
+
+		Assert.assertEquals(authClient.isLoggedIn(), random);
+	}
+
+	@Test
+	public void testLoggedInTime() {
+		MessageIO messageIO = mock(MessageIO.class);
+		ClientUser clientUser = mock(ClientUser.class);
+		long random = (long) (Math.random() * 1000);
+		AuthClient authClient = new AuthClient(messageIO, clientUser);
+		authClient.setLoggedInTime(random);
+
+		Assert.assertEquals(authClient.getLoggedInTime(), random);
+	}
+
+	@Test
+	public void testGetUser() {
+		MessageIO messageIO = mock(MessageIO.class);
+		ClientUser clientUser = mock(ClientUser.class);
+		AuthClient authClient = new AuthClient(messageIO, clientUser);
+
+		Assert.assertEquals(authClient.getUser(), clientUser);
+	}
+}
