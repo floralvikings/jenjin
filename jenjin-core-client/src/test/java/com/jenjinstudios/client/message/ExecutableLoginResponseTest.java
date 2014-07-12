7ce438a47f48863171ef3057b4e52dffdc6cdf2f
Caleb Brinkman <floralvikings@gmail.com>
2014-07-11 23:49:53 -0500

    Added ExecutableLogoutResponseTest

---
+++ jenjin-core-client/src/test/java/com/jenjinstudios/client/message/ExecutableLoginResponseTest.java
@@ -0,0 +1,33 @@
+package com.jenjinstudios.client.message;
+
+import com.jenjinstudios.client.net.AuthClient;
+import com.jenjinstudios.client.net.ClientUser;
+import com.jenjinstudios.core.io.Message;
+import com.jenjinstudios.core.io.MessageRegistry;
+import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Caleb Brinkman
+ */
+public class ExecutableLoginResponseTest
+{
+	@Test
+	public void testMessageExecution() {
+		Message loginResponse = MessageRegistry.getInstance().createMessage("LoginResponse");
+		loginResponse.setArgument("success", true);
+		loginResponse.setArgument("loginTime", 12345l);
+
+		ClientUser user = mock(ClientUser.class);
+		AuthClient authClient = mock(AuthClient.class);
+		when(authClient.isLoggedIn()).thenReturn(true);
+		when(authClient.getUser()).thenReturn(user);
+
+		ExecutableLoginResponse executableLoginResponse = new ExecutableLoginResponse(authClient, loginResponse);
+		executableLoginResponse.runImmediate();
+		executableLoginResponse.runDelayed();
+
+		verify(authClient).setLoggedInTime(12345l);
+	}
+}
