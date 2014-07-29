69e06b09b52980f968e95452c1e3a38bac9d0643
Caleb Brinkman <floralvikings@gmail.com>
2014-07-29 13:39:57 -0500

    Removed unused MessageRegistry reference

--- jenjin-core/src/main/java/com/jenjinstudios/core/MessageIO.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/MessageIO.java
@@ -2,7 +2,6 @@ package com.jenjinstudios.core;
 
 import com.jenjinstudios.core.io.MessageInputStream;
 import com.jenjinstudios.core.io.MessageOutputStream;
-import com.jenjinstudios.core.io.MessageRegistry;
 
 /**
  * @author Caleb Brinkman
@@ -11,17 +10,14 @@ public class MessageIO
 {
 	private final MessageInputStream in;
 	private final MessageOutputStream out;
-	private final MessageRegistry mr;
 
 	public MessageIO(MessageInputStream in, MessageOutputStream out) {
 		this.in = in;
 		this.out = out;
-		this.mr = MessageRegistry.getInstance();
 	}
 
 	public MessageInputStream getIn() { return in; }
 
 	public MessageOutputStream getOut() { return out; }
 
-	public MessageRegistry getMr() { return mr; }
 }
