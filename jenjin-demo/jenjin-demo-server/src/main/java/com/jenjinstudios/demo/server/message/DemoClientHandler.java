22af9d4c9820262fd80c9c243995ffb85058b657
Caleb Brinkman <floralvikings@gmail.com>
2014-07-28 22:51:58 -0500

    Removed redundant throws clause

--- jenjin-demo/jenjin-demo-server/src/main/java/com/jenjinstudios/demo/server/message/DemoClientHandler.java
+++ jenjin-demo/jenjin-demo-server/src/main/java/com/jenjinstudios/demo/server/message/DemoClientHandler.java
@@ -4,14 +4,12 @@ import com.jenjinstudios.core.MessageIO;
 import com.jenjinstudios.demo.server.DemoServer;
 import com.jenjinstudios.world.server.WorldClientHandler;
 
-import java.io.IOException;
-
 /**
  * @author Caleb Brinkman
  */
 public class DemoClientHandler extends WorldClientHandler
 {
-	public DemoClientHandler(DemoServer<? extends WorldClientHandler> s, MessageIO messageIO) throws IOException {
+	public DemoClientHandler(DemoServer<? extends WorldClientHandler> s, MessageIO messageIO) {
 		super(s, messageIO);
 		setPlayer(new DemoPlayer());
 	}
