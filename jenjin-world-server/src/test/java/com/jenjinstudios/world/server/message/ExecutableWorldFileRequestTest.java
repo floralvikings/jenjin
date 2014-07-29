e0281f87a7f441f5974941823721c9ea53dde5b2
Caleb Brinkman <floralvikings@gmail.com>
2014-07-28 20:53:52 -0500

    Suppressing warning

--- jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldFileRequestTest.java
+++ jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldFileRequestTest.java
@@ -13,6 +13,7 @@ import static org.mockito.Mockito.*;
  */
 public class ExecutableWorldFileRequestTest
 {
+	@SuppressWarnings("unchecked")
 	@Test
 	public void testExecuteMessage() {
 		Message message = mock(Message.class);
