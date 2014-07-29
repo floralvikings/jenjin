6bd10a22730fe59e6c76d2bd827f3b0f0c2d9484
Caleb Brinkman <floralvikings@gmail.com>
2014-07-28 22:32:08 -0500

    Removed properties

--- jenjin-world-server/src/main/java/com/jenjinstudios/world/server/Player.java
+++ jenjin-world-server/src/main/java/com/jenjinstudios/world/server/Player.java
@@ -3,30 +3,22 @@ package com.jenjinstudios.world.server;
 import com.jenjinstudios.world.Actor;
 import com.jenjinstudios.world.math.Angle;
 
-import java.util.TreeMap;
-
 /**
  * The player class represents a player in the server-side world.
  * @author Caleb Brinkman
  */
 public class Player extends Actor
 {
-	private final TreeMap<String, Object> properties;
 	private Angle pendingAngle;
 
 	public Player(String username) {
 		super(username);
 		setPendingAngle(getAngle());
-		properties = new TreeMap<>();
 	}
 
 	public Angle getPendingAngle() { return pendingAngle; }
 
 	public void setPendingAngle(Angle pendingAngle) { this.pendingAngle = pendingAngle; }
-
-	public void setProperty(String property, Object value) { properties.put(property, value); }
-
-	public Object getProperty(String property) { return properties.get(property); }
 
 	@Override
 	public void setUp() {
