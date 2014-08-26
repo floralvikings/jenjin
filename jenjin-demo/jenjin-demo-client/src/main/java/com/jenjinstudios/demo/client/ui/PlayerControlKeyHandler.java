4737e9bbb24572c0d007ea3144f956ad207119f1
Caleb Brinkman <floralvikings@gmail.com>
2014-08-25 20:01:23 -0500

    Added PlayerKeyFlags class

--- jenjin-demo/jenjin-demo-client/src/main/java/com/jenjinstudios/demo/client/ui/PlayerControlKeyHandler.java
+++ jenjin-demo/jenjin-demo-client/src/main/java/com/jenjinstudios/demo/client/ui/PlayerControlKeyHandler.java
@@ -6,10 +6,11 @@ import com.jenjinstudios.world.client.ClientPlayer;
 import com.jenjinstudios.world.client.WorldClient;
 import com.jenjinstudios.world.math.Angle;
 import javafx.event.EventHandler;
-import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 
-import static javafx.scene.input.KeyCode.*;
+import static com.jenjinstudios.demo.client.ui.PlayerKeyFlags.isFireKey;
+import static com.jenjinstudios.demo.client.ui.PlayerKeyFlags.isMovementKey;
+import static com.jenjinstudios.world.math.Angle.*;
 
 /**
  * @author Caleb Brinkman
@@ -18,18 +19,19 @@ public class PlayerControlKeyHandler implements EventHandler<KeyEvent>
 {
 	private final WorldClient worldClient;
 	private final ClientPlayer clientPlayer;
-	private boolean up, down, left, right;
+	private final PlayerKeyFlags flags;
 
 	public PlayerControlKeyHandler(WorldClient worldClient) {
 		this.worldClient = worldClient;
 		this.clientPlayer = worldClient.getPlayer();
+		this.flags = new PlayerKeyFlags();
 	}
 
 	@Override
 	public void handle(KeyEvent keyEvent) {
 		if (isMovementKey(keyEvent))
 		{
-			setKeyFlags(keyEvent);
+			flags.setKeyFlags(keyEvent);
 			setNewAngle();
 		} else if (isFireKey(keyEvent))
 		{
@@ -39,30 +41,22 @@ public class PlayerControlKeyHandler implements EventHandler<KeyEvent>
 	}
 
 	protected Angle getMoveAngle(Angle angle) {
-		if (upKey())
+		if (flags.upKey())
 		{
-			double absAngle = leftKey() ? Angle.BACK_LEFT : (rightKey() ? Angle.FRONT_LEFT : Angle.LEFT);
-			angle = new Angle(absAngle, Angle.FRONT);
-		} else if (downKey())
+			double absAngle = flags.leftKey() ? BACK_LEFT : (flags.rightKey() ? FRONT_LEFT : LEFT);
+			angle = new Angle(absAngle, FRONT);
+		} else if (flags.downKey())
 		{
-			double absAngle = leftKey() ? Angle.BACK_RIGHT : (rightKey() ? Angle.FRONT_RIGHT : Angle.RIGHT);
-			angle = new Angle(absAngle, Angle.FRONT);
-		} else if (leftKey())
+			double absAngle = flags.leftKey() ? BACK_RIGHT : (flags.rightKey() ? FRONT_RIGHT : RIGHT);
+			angle = new Angle(absAngle, FRONT);
+		} else if (flags.leftKey())
 		{
-			angle = new Angle(Angle.BACK, Angle.FRONT);
-		} else if (rightKey())
+			angle = new Angle(BACK, FRONT);
+		} else if (flags.rightKey())
 		{
-			angle = new Angle(Angle.FRONT, Angle.FRONT);
+			angle = new Angle(FRONT, FRONT);
 		}
 		return angle;
-	}
-
-	protected void setKeyFlags(KeyEvent keyEvent) {
-		KeyCode keyCode = keyEvent.getCode();
-		up = keyCode.equals(UP) || keyCode.equals(W) ? keyEvent.getEventType() == KeyEvent.KEY_PRESSED : up;
-		down = keyCode.equals(DOWN) || keyCode.equals(S) ? keyEvent.getEventType() == KeyEvent.KEY_PRESSED : down;
-		left = keyCode.equals(LEFT) || keyCode.equals(A) ? keyEvent.getEventType() == KeyEvent.KEY_PRESSED : left;
-		right = keyCode.equals(RIGHT) || keyCode.equals(D) ? keyEvent.getEventType() == KeyEvent.KEY_PRESSED : right;
 	}
 
 	private void sendFireRequest() {
@@ -70,27 +64,10 @@ public class PlayerControlKeyHandler implements EventHandler<KeyEvent>
 		worldClient.queueOutgoingMessage(message);
 	}
 
-	private boolean isFireKey(KeyEvent keyEvent) {
-		return keyEvent.getCode().equals(SPACE) && keyEvent.getEventType() == KeyEvent.KEY_PRESSED;
-	}
-
-	private boolean isMovementKey(KeyEvent keyEvent) {
-		KeyCode c = keyEvent.getCode();
-		return c.isArrowKey() || c.equals(W) || c.equals(A) || c.equals(S) || c.equals(D);
-	}
-
 	private void setNewAngle() {
 		Angle angle = clientPlayer.getAngle().asIdle();
 		angle = getMoveAngle(angle);
 		clientPlayer.setAngle(angle);
 	}
-
-	private boolean rightKey() {return right && !left;}
-
-	private boolean leftKey() {return left && !right;}
-
-	private boolean downKey() {return down && !up;}
-
-	private boolean upKey() {return up && !down;}
 
 }
