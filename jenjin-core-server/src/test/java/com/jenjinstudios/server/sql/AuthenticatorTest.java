6882b3ac5e2a8410c296a6540286f955b18e2be6
Caleb Brinkman <floralvikings@gmail.com>
2014-09-14 16:48:48 -0500

    Removed unused SQL records

--- jenjin-core-server/src/test/java/com/jenjinstudios/server/sql/AuthenticatorTest.java
+++ jenjin-core-server/src/test/java/com/jenjinstudios/server/sql/AuthenticatorTest.java
@@ -34,9 +34,6 @@ public class AuthenticatorTest
 			  "  `password` CHAR(64) NOT NULL," +
 			  "  `salt` CHAR(48) NOT NULL," +
 			  "  `loggedin` TINYINT NOT NULL DEFAULT '0'," +
-			  "  `xcoord` DOUBLE NOT NULL DEFAULT '0'," +
-			  "  `ycoord` DOUBLE NOT NULL DEFAULT '0'," +
-			  "  `zoneid` INT(11) NOT NULL DEFAULT '0'," +
 			  "  PRIMARY KEY (username)" +
 			  ")");
 		statement.executeUpdate("CREATE TABLE jenjin_user_properties (" +
@@ -48,12 +45,12 @@ public class AuthenticatorTest
 		{
 			statement.executeUpdate(
 				  "INSERT INTO jenjin_users " +
-						"(`username`, `password`, `salt`, `loggedin`, `xcoord`, `ycoord`, `zoneid`)" +
+						"(`username`, `password`, `salt`, `loggedin`)" +
 						" VALUES " +
 						"('TestAccount" + i + "', " +
 						"'650f00f552d4df0147d236e240ccfc490444f4b358c4ff1d79f5fd90f57243bd', " +
 						"'e3c42b85a183d3f654a3d2bb3bc5ea607d0fb529d9b890d3', " +
-						"'0', '0', '0', '0')");
+						"'0')");
 			statement.executeUpdate("INSERT INTO jenjin_user_properties (`username`, `propertyName`, " +
 				  "`propertyValue`) " +
 				  "VALUES ('TestAccount" + i + "', 'Foo', 'Bar')");
