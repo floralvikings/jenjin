c267125df163a665f2973eb0e02ddd34e1a312e0
Caleb Brinkman <floralvikings@gmail.com>
2014-10-07 23:44:18 -0400

    Added constructor JavaDoc

--- jenjin-core/src/main/java/com/jenjinstudios/core/io/MessageTypeException.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/io/MessageTypeException.java
@@ -11,6 +11,11 @@ public class MessageTypeException extends IOException
 {
 	private final short id;
 
+	/**
+	 * Construct a new {@code MessageTypeException} when an invalid message type is read with the given id.
+	 *
+	 * @param id The id of the invalid message type.
+	 */
 	public MessageTypeException(short id) {
 		super("Message " + id + " not registered.");
 		this.id = id;
