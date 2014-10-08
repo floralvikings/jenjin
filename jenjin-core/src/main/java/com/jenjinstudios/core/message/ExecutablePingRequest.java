54d981da4dcea53f05490d9edba27af2c76b7ce3
Caleb Brinkman <floralvikings@gmail.com>
2014-10-08 00:31:24 -0400

    Added class JavaDoc

--- jenjin-core/src/main/java/com/jenjinstudios/core/message/ExecutablePingRequest.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/message/ExecutablePingRequest.java
@@ -3,6 +3,11 @@ package com.jenjinstudios.core.message;
 import com.jenjinstudios.core.Connection;
 import com.jenjinstudios.core.io.Message;
 
+/**
+ * Used to determine the time taken to send a "ping" to a connection.
+ *
+ * @author Caleb Brinkman
+ */
 public class ExecutablePingRequest extends ExecutableMessage
 {
 	private final Connection connection;
