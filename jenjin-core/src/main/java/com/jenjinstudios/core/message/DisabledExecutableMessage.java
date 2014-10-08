e454711e0bede31e461d0d9a62f227c43e98d6ce
Caleb Brinkman <floralvikings@gmail.com>
2014-10-08 00:17:04 -0400

    Removed existing JavaDocs

--- jenjin-core/src/main/java/com/jenjinstudios/core/message/DisabledExecutableMessage.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/message/DisabledExecutableMessage.java
@@ -6,17 +6,10 @@ import com.jenjinstudios.core.io.Message;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * @author Caleb Brinkman
- */
 public class DisabledExecutableMessage extends ExecutableMessage
 {
 	private static final Logger LOGGER = Logger.getLogger(DisabledExecutableMessage.class.getName());
 
-	/**
-	 * Construct an ExecutableMessage with the given Message.
-	 * @param message The Message.
-	 */
 	public DisabledExecutableMessage(Connection connection, Message message) {
 		super(message);
 		LOGGER.log(Level.SEVERE, "DisabledExecutableMessage constructed in {0}", connection);
