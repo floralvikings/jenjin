6a44d176f93308d71b899e82de001627bd1e3c8e
Caleb Brinkman <floralvikings@gmail.com>
2014-10-09 19:40:18 -0400

    Added class JavaDoc

--- jenjin-core/src/main/java/com/jenjinstudios/core/xml/DisabledMessageType.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/xml/DisabledMessageType.java
@@ -5,6 +5,11 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
+/**
+ * Represents a type of {@code Message} that should be disabled, and not invoked when received.
+ *
+ * @author Caleb Brinkman
+ */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "disabled_message", namespace = "https://www.jenjinstudios.com")
 public class DisabledMessageType
