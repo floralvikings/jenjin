9a2ad4506bca039c4480bd373f9ed5f4c95e6903
Caleb Brinkman <floralvikings@gmail.com>
2014-10-09 19:39:44 -0400

    Removed existing JavaDoc

--- jenjin-core/src/main/java/com/jenjinstudios/core/xml/DisabledMessageType.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/xml/DisabledMessageType.java
@@ -5,24 +5,6 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
-
-/**
- * <p>Java class for disabled_messageType complex type.
- * <p>
- * <p>The following schema fragment specifies the expected content contained within this class.
- * <p>
- * <pre>
- * &lt;complexType name="disabled_messageType">
- *   &lt;simpleContent>
- *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
- *       &lt;attribute name="name" type="{http://www.w3.org/2001/XMLSchema}string" />
- *     &lt;/extension>
- *   &lt;/simpleContent>
- * &lt;/complexType>
- * </pre>
- *
- * @author Caleb Brinkman
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "disabled_message", namespace = "https://www.jenjinstudios.com")
 public class DisabledMessageType
@@ -31,10 +13,6 @@ public class DisabledMessageType
 	@XmlAttribute(name = "name")
 	private String name;
 
-	/**
-	 * Gets the value of the name property.
-	 * @return possible object is {@link String }
-	 */
 	public String getName() {
 		return name;
 	}
