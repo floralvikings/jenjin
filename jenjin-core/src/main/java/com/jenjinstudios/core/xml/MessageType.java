23f87545b34f7ba9f8ad462de61fd657a5877032
Caleb Brinkman <floralvikings@gmail.com>
2014-10-09 19:45:15 -0400

    Removed existing JavaDocs

--- jenjin-core/src/main/java/com/jenjinstudios/core/xml/MessageType.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/xml/MessageType.java
@@ -4,27 +4,6 @@ import javax.xml.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-
-/**
- * <p>Java class for messageType complex type.
- * <p>
- * <p>The following schema fragment specifies the expected content contained within this class.
- * <p>
- * <pre>
- * &lt;complexType name="messageType">
- *   &lt;complexContent>
- *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
- *       &lt;sequence>
- *         &lt;element name="arguments" type="{}argumentType" maxOccurs="unbounded" minOccurs="0"/>
- *         &lt;element name="executable" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
- *       &lt;/sequence>
- *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
- *       &lt;attribute name="id" use="required" type="{http://www.w3.org/2001/XMLSchema}byte" />
- *     &lt;/restriction>
- *   &lt;/complexContent>
- * &lt;/complexType>
- * </pre>
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "message", namespace = "https://www.jenjinstudios.com", propOrder = {
 	  "arguments",
@@ -41,25 +20,6 @@ public class MessageType
 	@XmlAttribute(name = "id", required = true)
 	private short id;
 
-	/**
-	 * Gets the value of the arguments property.
-	 * <p>
-	 * <p>
-	 * This accessor method returns a reference to the live list, not a snapshot. Therefore any modification you
-	 * make to
-	 * the returned list will be present inside the JAXB object. This is why there is not a <CODE>set</CODE> method for
-	 * the arguments property.
-	 * <p>
-	 * <p>
-	 * For example, to add a new item, do as follows:
-	 * <pre>
-	 *    getArguments().add(newItem);
-	 * </pre>
-	 * <p>
-	 * <p>
-	 * <p>
-	 * Objects of the following type(s) are allowed in the list {@link ArgumentType }
-	 */
 	public List<ArgumentType> getArguments() {
 		if (arguments == null)
 		{
@@ -68,32 +28,14 @@ public class MessageType
 		return this.arguments;
 	}
 
-	/**
-	 * Gets the value of the executable property.
-	 * @return possible object is {@link String }
-	 */
 	public String getExecutable() { return executable; }
 
-	/**
-	 * Gets the value of the name property.
-	 * @return possible object is {@link String }
-	 */
 	public String getName() { return name; }
 
-	/**
-	 * Sets the value of the name property.
-	 * @param value allowed object is {@link String }
-	 */
 	public void setName(String value) { this.name = value; }
 
-	/**
-	 * Gets the value of the id property.
-	 */
 	public short getId() { return id; }
 
-	/**
-	 * Sets the value of the id property.
-	 */
 	public void setId(short value) { this.id = value; }
 
 }
