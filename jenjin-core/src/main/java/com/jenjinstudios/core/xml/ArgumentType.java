515f0ddc2a04bb87863dd37019c564feb786d4e8
Caleb Brinkman <floralvikings@gmail.com>
2014-10-09 19:29:25 -0400

    Removed existing JavaDocs

--- jenjin-core/src/main/java/com/jenjinstudios/core/xml/ArgumentType.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/xml/ArgumentType.java
@@ -6,40 +6,6 @@ import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlType;
 
 
-/**
- * <p>Java class for argumentType complex type.
- * <p>
- * <p>The following schema fragment specifies the expected content contained within this class.
- * <p>
- * <pre>
- * &lt;complexType name="argumentType">
- *   &lt;simpleContent>
- *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>string">
- *       &lt;attribute name="type" use="required">
- *         &lt;simpleType>
- *           &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
- *             &lt;enumeration value="byte[]"/>
- *             &lt;enumeration value="String[]"/>
- *             &lt;enumeration value="boolean"/>
- *             &lt;enumeration value="byte"/>
- *             &lt;enumeration value="short"/>
- *             &lt;enumeration value="int"/>
- *             &lt;enumeration value="long"/>
- *             &lt;enumeration value="float"/>
- *             &lt;enumeration value="double"/>
- *             &lt;enumeration value="String"/>
- *           &lt;/restriction>
- *         &lt;/simpleType>
- *       &lt;/attribute>
- *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" />
- *       &lt;attribute name="encrypt" type="{http://www.w3.org/2001/XMLSchema}boolean" />
- *     &lt;/extension>
- *   &lt;/simpleContent>
- * &lt;/complexType>
- * </pre>
- *
- * @author Caleb Brinkman
- */
 @XmlAccessorType(XmlAccessType.FIELD)
 @XmlType(name = "argument", namespace = "https://www.jenjinstudios.com")
 public class ArgumentType
@@ -51,22 +17,10 @@ public class ArgumentType
 	@XmlAttribute(name = "encrypt")
 	private boolean encrypt = false;
 
-	/**
-	 * Gets the value of the type property.
-	 * @return possible object is {@link String }
-	 */
 	public String getType() { return type; }
 
-	/**
-	 * Gets the value of the name property.
-	 * @return possible object is {@link String }
-	 */
 	public String getName() { return name; }
 
-	/**
-	 * Gets the value of the encrypt property.
-	 * @return possible object is {@link Boolean }
-	 */
 	public Boolean isEncrypt() { return encrypt; }
 
 	@Override
