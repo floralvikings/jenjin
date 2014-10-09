6eecf78374d3c7760688e84dce3150d29db1b8fe
Caleb Brinkman <floralvikings@gmail.com>
2014-10-09 13:06:28 -0400

    Removed existing JavaDoc

--- jenjin-core/src/main/java/com/jenjinstudios/core/util/Hash.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/util/Hash.java
@@ -4,17 +4,8 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-/**
- * The {@code Hash} class contains methods for getting encrypted strings.
- * @author Caleb Brinkman
- */
 public class Hash
 {
-	/**
-	 * Return a SHA1 hash of the given string.
-	 * @param input The string to hash.
-	 * @return a SHA1 hash of the given string.
-	 */
 	private static String getHashedString(String input) {
 		String hashedString;
 		try
@@ -41,12 +32,6 @@ public class Hash
 		return hashedString;
 	}
 
-	/**
-	 * Get a salted, SHA256 hashed string for the supplied string and salt.
-	 * @param input The string to be hashed.
-	 * @param salt The salt to be applied before hashing.
-	 * @return The hashed and salted string.
-	 */
 	public static String getHashedString(String input, String salt) {
 		return getHashedString(salt + input);
 	}
