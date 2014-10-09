2a5d973f6c62be87430a491e0973138511662e7a
Caleb Brinkman <floralvikings@gmail.com>
2014-10-09 13:02:05 -0400

    Removed existing JavaDoc

--- jenjin-core/src/main/java/com/jenjinstudios/core/util/Files.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/util/Files.java
@@ -3,20 +3,10 @@ package com.jenjinstudios.core.util;
 import java.io.File;
 import java.util.ArrayList;
 
-/**
- * The {@code Files} is used to get the MD5 hash checksum from a file.
- * @author Caleb Brinkman
- */
 public class Files
 {
 	private Files() { }
 
-	/**
-	 * Search a directory and subdirectories for files with the given name.
-	 * @param dir The directory in which to start looking.
-	 * @param fileName The name of the file(s) for which to look.
-	 * @return An ArrayList of files in the given directory or a subdirectory, with the supplied file name.
-	 */
 	public static ArrayList<File> search(File dir, String fileName) {
 		ArrayList<File> files = new ArrayList<>();
 		File[] contents = dir.listFiles();
