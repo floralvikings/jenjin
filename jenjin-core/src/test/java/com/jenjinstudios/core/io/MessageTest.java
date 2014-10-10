17c7b333511083dfe918330cc523ebc87b8d26af
Caleb Brinkman <floralvikings@gmail.com>
2014-10-10 17:56:24 -0400

    Split test messages into separate file in tests directory; tweaked tests slightly to fix altered ID numbers. Added JavaDocs to test files

--- jenjin-core/src/test/java/com/jenjinstudios/core/io/MessageTest.java
+++ jenjin-core/src/test/java/com/jenjinstudios/core/io/MessageTest.java
@@ -4,18 +4,25 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 /**
+ * Tests the {@code Message class}.
  * @author Caleb Brinkman
  */
 public class MessageTest
 {
 	private static final MessageRegistry mr = MessageRegistry.getInstance();
 
+	/**
+	 * Test creating a message with an invalid name.
+	 */
 	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testInvalidName() {
 		Message msg = mr.createMessage("InvalidMessage");
 		msg.setArgument("FooBar", 1337); // Expect exception
 	}
 
+	/**
+	 * Test creating a message with an invalid argument.
+	 */
 	@Test(expectedExceptions = IllegalArgumentException.class)
 	public void testInvalidArgumentType() {
 		Message msg = mr.createMessage("InvalidMessage");
@@ -23,6 +30,9 @@ public class MessageTest
 		msg.setArgument("messageID", "I'm totally a short, you guys."); // Expect exception
 	}
 
+	/**
+	 * Test retrieving message arguments with arguments unset.
+	 */
 	@Test(expectedExceptions = IllegalStateException.class)
 	public void testUnsetArgs() {
 		Message msg = mr.createMessage("InvalidMessage");
@@ -30,20 +40,26 @@ public class MessageTest
 		msg.getArgs(); // Expect exception
 	}
 
+	/**
+	 * Test message toString method.
+	 */
 	@Test
 	public void testToString() {
 		Message msg = mr.createMessage("InvalidMessage");
 		msg.setArgument("messageName", "FooBar");
 		msg.setArgument("messageID", (short) -255);
 
-		String expected = "Message -1 InvalidMessage";
+		String expected = "Message 0 InvalidMessage";
 		String actual = msg.toString();
 
 		Assert.assertEquals(actual, expected);
 	}
 
+	/**
+	 * Test constructor used in streams.
+	 */
 	@Test(expectedExceptions = IllegalStateException.class)
 	public void testStreamConstructor() {
-		new Message(mr, (short) -1, "Bob"); // Expect Exception
+		new Message(mr, (short) 0, "Bob"); // Expect Exception
 	}
 }
