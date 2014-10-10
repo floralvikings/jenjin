dc32cdb8821484c10f2f7e5e72d7cd08a19701ab
Caleb Brinkman <floralvikings@gmail.com>
2014-10-10 18:05:31 -0400

    Refactored, extracting methods and renaming.

--- jenjin-core/src/main/java/com/jenjinstudios/core/util/HashUtil.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/util/HashUtil.java
@@ -13,30 +13,37 @@ public class HashUtil
 {
 	// TODO Modify this method to be more flexible.
 	// TODO Rename to something that reflects that this function returns a SHA-256 hash.
-	private static String getHashedString(String input) {
+	private static String getSHA256String(String input) {
 		try
 		{
-			String hashedString;
 			//Convert the pass to an md5 hash string
-			byte[] passBytes = input.getBytes("UTF-8");
-			MessageDigest md = MessageDigest.getInstance("SHA-256");
-			byte[] encryption = md.digest(passBytes);
-			StringBuilder hexString = new StringBuilder();
-			for (byte anEncryption : encryption)
-			{ // Convert back to a string, making sure to include leading zeros.
-				String hex = Integer.toHexString(0xff & anEncryption);
-				if (hex.length() == 1)
-				{
-					hexString.append('0');
-				}
-				hexString.append(hex);
-			}
-			hashedString = hexString.toString();
-			return hashedString;
+			return getFullHexString(getSHA256Hash(input));
 		} catch (NoSuchAlgorithmException | UnsupportedEncodingException ex)
 		{
 			throw new RuntimeException("Unable to find SHA-256 Algorithm");
 		}
+	}
+
+	private static byte[] getSHA256Hash(String input) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+		byte[] passBytes = input.getBytes("UTF-8");
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+		return md.digest(passBytes);
+	}
+
+	private static String getFullHexString(byte[] bytes) {
+		String hashedString;
+		StringBuilder hexString = new StringBuilder();
+		for (byte anEncryption : bytes)
+		{ // Convert back to a string, making sure to include leading zeros.
+			String hex = Integer.toHexString(0xff & anEncryption);
+			if (hex.length() == 1)
+			{
+				hexString.append('0');
+			}
+			hexString.append(hex);
+		}
+		hashedString = hexString.toString();
+		return hashedString;
 	}
 
 	/**
@@ -49,6 +56,6 @@ public class HashUtil
 	 */
 	// TODO Rename to reflect that this function uses SHA-256
 	public static String getHashedString(String input, String salt) {
-		return getHashedString(salt + input);
+		return getSHA256String(salt + input);
 	}
 }
