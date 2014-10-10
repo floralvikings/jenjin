5c3a80b6d3497a963a9307a5ae231922c0b1d99a
Caleb Brinkman <floralvikings@gmail.com>
2014-10-10 17:32:37 -0400

    Added TODO comment

--- jenjin-core-client/src/main/java/com/jenjinstudios/client/message/ClientExecutableMessage.java
+++ jenjin-core-client/src/main/java/com/jenjinstudios/client/message/ClientExecutableMessage.java
@@ -1,13 +1,14 @@
 package com.jenjinstudios.client.message;
 
-import com.jenjinstudios.core.message.ExecutableMessage;
-import com.jenjinstudios.core.io.Message;
 import com.jenjinstudios.client.net.Client;
+import com.jenjinstudios.core.io.Message;
+import com.jenjinstudios.core.message.ExecutableMessage;
 
 /**
  * Superclass of ExecutableMessages used by the client.
  * @author Caleb Brinkman
  */
+// TODO This class can probably be refactored away; should the subclasses just cast the constructor arguments?
 public abstract class ClientExecutableMessage extends ExecutableMessage
 {
 	/** The client invoking this ExecutableMessage. */
