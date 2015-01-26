094509443485c0b4fdbca7efe27d90ea9e65454e
Caleb Brinkman <Caleb.Brinkman@whitehatsec.com>
2015-01-25 21:51:15 -0600

    Removed now-unnecessary reading of extra public key message.

--- jenjin-core/src/test/java/com/jenjinstudios/core/ConnectionTest.java
+++ jenjin-core/src/test/java/com/jenjinstudios/core/ConnectionTest.java
@@ -125,7 +125,6 @@ public class ConnectionTest
 		// The connection should execute the InvalidExecutableMessage,
 		byte[] bytes = bos.toByteArray();
 		MessageInputStream mis = new MessageInputStream(new ByteArrayInputStream(bytes));
-		mis.readMessage(); // Read the public key message
 		Message msg = mis.readMessage();
 		Assert.assertEquals(msg.name, "PingResponse");
 	}
