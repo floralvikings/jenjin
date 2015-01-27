f6fae2c568a2c9e71d528f40676b6c9b1b7d8391
Caleb Brinkman <Caleb.Brinkman@whitehatsec.com>
2015-01-27 17:29:53 -0600

    Added empty ServerInitTest

---
+++ jenjin-core-server/src/test/java/com/jenjinstudios/server/net/ServerInitTest.java
@@ -0,0 +1,19 @@
+package com.jenjinstudios.server.net;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ServerInitTest
+{
+
+	@Test
+	public void testReadFromFile() throws Exception {
+
+	}
+
+	@Test
+	public void testReadFromStream() throws Exception {
+
+	}
+}
