1231e1de86823828ff2483c57a11d82af2e8d483
Caleb Brinkman <floralvikings@gmail.com>
2015-02-10 17:36:31 -0600

    Improved error handling, proper stream closure

--- jenjin-core/src/main/java/com/jenjinstudios/core/io/MessageRegistryReader.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/io/MessageRegistryReader.java
@@ -3,7 +3,10 @@ package com.jenjinstudios.core.io;
 import com.jenjinstudios.core.xml.MessageGroup;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -29,11 +32,24 @@ public class MessageRegistryReader
 				Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 				MessageGroup collection = (MessageGroup) jaxbUnmarshaller.unmarshal(inputStream);
 				foundMessages.add(collection);
-			} catch (Exception ex)
-			{
-				LOGGER.log(Level.INFO, "Unable to parse XML file", ex);
-			}
-		}
-		return foundMessages;
+            } catch (JAXBException | RuntimeException ex)
+            {
+                LOGGER.log(Level.INFO, "Unable to parse XML file", ex);
+            } finally
+            {
+                closeQuietly(inputStream);
+            }
+        }
+        return foundMessages;
 	}
+
+    private static void closeQuietly(Closeable inputStream) {
+        try
+        {
+            inputStream.close();
+        } catch (IOException ex)
+        {
+            LOGGER.log(Level.INFO, "Unable to close stream", ex);
+        }
+    }
 }
