8c8a0a7d30c01380e0f9391dc60340b3e40fbdd2
Caleb Brinkman <floralvikings@gmail.com>
2015-02-11 00:09:30 -0600

    Suppressing magic number inspections

--- jenjin-core/src/test/java/com/jenjinstudios/core/io/MessageTest.java
+++ jenjin-core/src/test/java/com/jenjinstudios/core/io/MessageTest.java
@@ -8,59 +8,60 @@ import org.testng.annotations.Test;
  *
  * @author Caleb Brinkman
  */
+@SuppressWarnings("MagicNumber")
 public class MessageTest
 {
-	private static final MessageRegistry mr = MessageRegistry.getInstance();
+    private static final MessageRegistry mr = MessageRegistry.getInstance();
 
-	/**
-	 * Test creating a message with an invalid name.
-	 */
-	@Test(expectedExceptions = IllegalArgumentException.class)
-	public void testInvalidName() {
-		Message msg = mr.createMessage("InvalidMessage");
-		msg.setArgument("FooBar", 1337); // Expect exception
-	}
+    /**
+     * Test creating a message with an invalid name.
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testInvalidName() {
+        Message msg = mr.createMessage("InvalidMessage");
+        msg.setArgument("FooBar", 1337); // Expect exception
+    }
 
-	/**
-	 * Test creating a message with an invalid argument.
-	 */
-	@Test(expectedExceptions = IllegalArgumentException.class)
-	public void testInvalidArgumentType() {
-		Message msg = mr.createMessage("InvalidMessage");
-		msg.setArgument("messageName", "FooBar");
-		msg.setArgument("messageID", "I'm totally a short, you guys."); // Expect exception
-	}
+    /**
+     * Test creating a message with an invalid argument.
+     */
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void testInvalidArgumentType() {
+        Message msg = mr.createMessage("InvalidMessage");
+        msg.setArgument("messageName", "FooBar");
+        msg.setArgument("messageID", "I'm totally a short, you guys."); // Expect exception
+    }
 
-	/**
-	 * Test retrieving message arguments with arguments unset.
-	 */
-	@Test(expectedExceptions = IllegalStateException.class)
-	public void testUnsetArgs() {
-		Message msg = mr.createMessage("InvalidMessage");
-		msg.setArgument("messageName", "FooBar");
-		msg.getArgs(); // Expect exception
-	}
+    /**
+     * Test retrieving message arguments with arguments unset.
+     */
+    @Test(expectedExceptions = IllegalStateException.class)
+    public void testUnsetArgs() {
+        Message msg = mr.createMessage("InvalidMessage");
+        msg.setArgument("messageName", "FooBar");
+        msg.getArgs(); // Expect exception
+    }
 
-	/**
-	 * Test message toString method.
-	 */
-	@Test
-	public void testToString() {
-		Message msg = mr.createMessage("InvalidMessage");
-		msg.setArgument("messageName", "FooBar");
-		msg.setArgument("messageID", (short) -255);
+    /**
+     * Test message toString method.
+     */
+    @Test
+    public void testToString() {
+        Message msg = mr.createMessage("InvalidMessage");
+        msg.setArgument("messageName", "FooBar");
+        msg.setArgument("messageID", (short) -255);
 
-		String expected = "Message 0 InvalidMessage";
-		String actual = msg.toString();
+        String expected = "Message 0 InvalidMessage";
+        String actual = msg.toString();
 
-		Assert.assertEquals(actual, expected);
-	}
+        Assert.assertEquals(actual, expected);
+    }
 
-	/**
-	 * Test constructor used in streams.
-	 */
+    /**
+     * Test constructor used in streams.
+     */
     @Test(expectedExceptions = IllegalArgumentException.class)
     public void testStreamConstructor() {
-		new Message((short) 0, "Bob"); // Expect Exception
-	}
+        new Message((short) 0, "Bob"); // Expect Exception
+    }
 }
