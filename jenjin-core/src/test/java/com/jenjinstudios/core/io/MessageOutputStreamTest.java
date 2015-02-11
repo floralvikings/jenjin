77900d7315f18bea07d8ef82bbc61901fcd220f6
Caleb Brinkman <floralvikings@gmail.com>
2015-02-11 00:27:34 -0600

    Made method static

--- jenjin-core/src/test/java/com/jenjinstudios/core/io/MessageOutputStreamTest.java
+++ jenjin-core/src/test/java/com/jenjinstudios/core/io/MessageOutputStreamTest.java
@@ -21,119 +21,119 @@ public class MessageOutputStreamTest
     private static final MessageRegistry MESSAGE_REGISTRY = MessageRegistry.getInstance();
     private static final Logger LOGGER = Logger.getLogger(MessageOutputStreamTest.class.getName());
 
-	/**
-	 * Test writing a message to the stream.
-	 *
-	 * @throws Exception If there's an exception.
-	 */
-	@Test
-	public void testWriteMessage() throws Exception {
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		MessageOutputStream mos = new MessageOutputStream(bos);
+    /**
+     * Test writing a message to the stream.
+     *
+     * @throws Exception If there's an exception.
+     */
+    @Test
+    public void testWriteMessage() throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        MessageOutputStream mos = new MessageOutputStream(bos);
 
         Message msg = MESSAGE_REGISTRY.createMessage("InvalidMessage");
         msg.setArgument("messageID", (short) -255);
-		msg.setArgument("messageName", "FooBar");
+        msg.setArgument("messageName", "FooBar");
 
-		mos.writeMessage(msg);
-		byte[] bytes = bos.toByteArray();
-		mos.close();
+        mos.writeMessage(msg);
+        byte[] bytes = bos.toByteArray();
+        mos.close();
 
-		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-		MessageInputStream mis = new MessageInputStream(bis);
-		Message readMsg = mis.readMessage();
-		Assert.assertEquals(readMsg.getArgs(), msg.getArgs());
-	}
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        MessageInputStream mis = new MessageInputStream(bis);
+        Message readMsg = mis.readMessage();
+        Assert.assertEquals(readMsg.getArgs(), msg.getArgs());
+    }
 
-	/**
-	 * Test writing an encrypted message with no key sent.
-	 *
-	 * @throws Exception If there's an exception.
-	 */
+    /**
+     * Test writing an encrypted message with no key sent.
+     *
+     * @throws Exception If there's an exception.
+     */
     @Test(expectedExceptions = IOException.class)
     public void testEncryptedMessageNoPublicKey() throws Exception {
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		MessageOutputStream mos = new MessageOutputStream(bos);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        MessageOutputStream mos = new MessageOutputStream(bos);
 
         Message msg = MESSAGE_REGISTRY.createMessage("TestEncryptedMessage");
         msg.setArgument("encryptedString", "FooBar");
 
-		mos.writeMessage(msg);
-	}
+        mos.writeMessage(msg);
+    }
 
-	/**
-	 * Test sending a correct encrypted message.
-	 *
-	 * @throws Exception If there's an exception.
-	 */
-	@Test
-	public void testEncryptedMessage() throws Exception {
+    /**
+     * Test sending a correct encrypted message.
+     *
+     * @throws Exception If there's an exception.
+     */
+    @Test
+    public void testEncryptedMessage() throws Exception {
 
-		KeyPair keyPair = generateRSAKeyPair();
+        KeyPair keyPair = generateRSAKeyPair();
 
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		MessageOutputStream mos = new MessageOutputStream(bos);
-		mos.setPublicKey(keyPair.getPublic());
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        MessageOutputStream mos = new MessageOutputStream(bos);
+        mos.setPublicKey(keyPair.getPublic());
 
         Message msg = MESSAGE_REGISTRY.createMessage("TestEncryptedMessage");
         msg.setArgument("encryptedString", "FooBar");
 
-		mos.writeMessage(msg);
-		byte[] bytes = bos.toByteArray();
-		mos.close();
+        mos.writeMessage(msg);
+        byte[] bytes = bos.toByteArray();
+        mos.close();
 
         DataInput dis = new DataInputStream(new ByteArrayInputStream(bytes));
         short id = dis.readShort();
-		boolean encrypted = dis.readBoolean();
-		String encStr = dis.readUTF();
+        boolean encrypted = dis.readBoolean();
+        String encStr = dis.readUTF();
 
-		Assert.assertEquals(id, msg.getID());
-		Assert.assertTrue(encrypted);
-		Assert.assertNotEquals(encStr, msg.getArgument("encryptedString"));
+        Assert.assertEquals(id, msg.getID());
+        Assert.assertTrue(encrypted);
+        Assert.assertNotEquals(encStr, msg.getArgument("encryptedString"));
 
-	}
+    }
 
-	/**
-	 * Test writing a message with each type of argument.
-	 *
-	 * @throws Exception If there's an exception.
-	 */
-	@Test
-	public void testAllTypesMessage() throws Exception {
-		ByteArrayOutputStream bos = new ByteArrayOutputStream();
-		MessageOutputStream mos = new MessageOutputStream(bos);
+    /**
+     * Test writing a message with each type of argument.
+     *
+     * @throws Exception If there's an exception.
+     */
+    @Test
+    public void testAllTypesMessage() throws Exception {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        MessageOutputStream mos = new MessageOutputStream(bos);
         Message msg = MESSAGE_REGISTRY.createMessage("TestAllTypesMessage");
         msg.setArgument("testString", "SNAFU");
-		msg.setArgument("testInt", 123);
-		msg.setArgument("testLong", 456l);
-		msg.setArgument("testDouble", 4.567);
-		msg.setArgument("testFloat", 0.123f);
-		msg.setArgument("testShort", (short) 789);
-		msg.setArgument("testBoolean", true);
-		msg.setArgument("testByte", (byte) 101);
-		msg.setArgument("testByteArray", new byte[]{2, 3, 5, 7, 11});
-		msg.setArgument("testStringArray", new String[]{"Foo", "Bar"});
-		mos.writeMessage(msg);
-		byte[] bytes = bos.toByteArray();
-		mos.close();
+        msg.setArgument("testInt", 123);
+        msg.setArgument("testLong", 456l);
+        msg.setArgument("testDouble", 4.567);
+        msg.setArgument("testFloat", 0.123f);
+        msg.setArgument("testShort", (short) 789);
+        msg.setArgument("testBoolean", true);
+        msg.setArgument("testByte", (byte) 101);
+        msg.setArgument("testByteArray", new byte[]{2, 3, 5, 7, 11});
+        msg.setArgument("testStringArray", new String[]{"Foo", "Bar"});
+        mos.writeMessage(msg);
+        byte[] bytes = bos.toByteArray();
+        mos.close();
 
-		ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-		MessageInputStream mis = new MessageInputStream(bis);
-		Message readMsg = mis.readMessage();
-		Assert.assertEquals(readMsg.getArgs(), msg.getArgs());
-	}
+        ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
+        MessageInputStream mis = new MessageInputStream(bis);
+        Message readMsg = mis.readMessage();
+        Assert.assertEquals(readMsg.getArgs(), msg.getArgs());
+    }
 
-	private KeyPair generateRSAKeyPair() {
-		KeyPair keyPair = null;
-		try
-		{
+    private static KeyPair generateRSAKeyPair() {
+        KeyPair keyPair = null;
+        try
+        {
             KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
             keyPairGenerator.initialize(512);
             keyPair = keyPairGenerator.generateKeyPair();
-		} catch (NoSuchAlgorithmException e)
-		{
-			LOGGER.log(Level.SEVERE, "Unable to create RSA key pair!", e);
-		}
-		return keyPair;
-	}
+        } catch (NoSuchAlgorithmException e)
+        {
+            LOGGER.log(Level.SEVERE, "Unable to create RSA key pair!", e);
+        }
+        return keyPair;
+    }
 }
