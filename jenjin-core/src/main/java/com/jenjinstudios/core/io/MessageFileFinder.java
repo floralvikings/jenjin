ef5192ef069df955ecfb675d1e3d4e93f9730b06
Caleb Brinkman <floralvikings@gmail.com>
2015-02-10 18:20:36 -0600

    Folded MessageRegistryReader into MessageFileFinder class because it wasn't like that already for some reason

--- jenjin-core/src/main/java/com/jenjinstudios/core/io/MessageFileFinder.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/io/MessageFileFinder.java
@@ -3,6 +3,9 @@ package com.jenjinstudios.core.io;
 import com.jenjinstudios.core.util.FileUtil;
 import com.jenjinstudios.core.xml.MessageGroup;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.Collection;
@@ -36,48 +39,85 @@ public final class MessageFileFinder
     private static Iterable<String> findJarMessageEntries() {
         Collection<String> jarMessageEntries = new LinkedList<>();
         String classPath = System.getProperty("java.class.path");
-		String[] pathElements = classPath.split(System.getProperty("path.separator"));
-		for (String fileName : pathElements)
-		{
+        String[] pathElements = classPath.split(System.getProperty("path.separator"));
+        for (String fileName : pathElements)
+        {
             if (!isCoreJar(fileName))
             {
                 seachJarFile(jarMessageEntries, fileName);
             }
 
         }
-		return jarMessageEntries;
-	}
+        return jarMessageEntries;
+    }
 
     private static boolean isCoreJar(String fileName) {
         String javaHome = System.getProperty("java.home");
-		return fileName.contains(javaHome);
-	}
+        return fileName.contains(javaHome);
+    }
 
     private static void seachJarFile(Collection<String> jarMessageEntries, String fileName) {
         File file = new File(fileName);
-		if (!file.isDirectory() && file.exists())
-		{
-			try (FileInputStream inputStream = new FileInputStream(file);
-				 ZipInputStream zip = new ZipInputStream(inputStream))
-			{
-				searchZipEntries(jarMessageEntries, zip);
-				inputStream.close();
-				zip.close();
-			} catch (IOException ex)
-			{
-				LOGGER.log(Level.WARNING, "Unable to read JAR entry " + fileName, ex);
-			}
-		}
-	}
+        if (!file.isDirectory() && file.exists())
+        {
+            try (FileInputStream inputStream = new FileInputStream(file);
+                 ZipInputStream zip = new ZipInputStream(inputStream))
+            {
+                searchZipEntries(jarMessageEntries, zip);
+                inputStream.close();
+                zip.close();
+            } catch (IOException ex)
+            {
+                LOGGER.log(Level.WARNING, "Unable to read JAR entry " + fileName, ex);
+            }
+        }
+    }
 
     private static void searchZipEntries(Collection<String> jarMessageEntries, ZipInputStream zip) throws IOException {
         ZipEntry ze;
-		while ((ze = zip.getNextEntry()) != null)
-		{
-			String entryName = ze.getName();
-			if (entryName.endsWith("Messages.xml")) { jarMessageEntries.add(entryName); }
-		}
-	}
+        while ((ze = zip.getNextEntry()) != null)
+        {
+            String entryName = ze.getName();
+            if (entryName.endsWith("Messages.xml")) { jarMessageEntries.add(entryName); }
+        }
+    }
+
+    private static Collection<MessageGroup> readXmlStreams(Iterable<InputStream> streamsToRead) {
+        Collection<MessageGroup> foundMessages = new LinkedList<>();
+        for (InputStream inputStream : streamsToRead)
+        {
+            try
+            {
+                JAXBContext jaxbContext = JAXBContext.newInstance(MessageGroup.class);
+                Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+                MessageGroup collection = (MessageGroup) jaxbUnmarshaller.unmarshal(inputStream);
+                if (foundMessages.contains(collection))
+                {
+                    LOGGER.log(Level.FINE, "Found duplicate message file; ignoring.");
+                } else
+                {
+                    foundMessages.add(collection);
+                }
+            } catch (JAXBException | RuntimeException ex)
+            {
+                LOGGER.log(Level.INFO, "Unable to parse XML file", ex);
+            } finally
+            {
+                closeQuietly(inputStream);
+            }
+        }
+        return foundMessages;
+    }
+
+    private static void closeQuietly(Closeable inputStream) {
+        try
+        {
+            inputStream.close();
+        } catch (IOException ex)
+        {
+            LOGGER.log(Level.INFO, "Unable to close stream", ex);
+        }
+    }
 
     private Iterable<File> findMessageFiles() {
         File rootFile = new File(rootDir);
@@ -91,7 +131,7 @@ public final class MessageFileFinder
         {
             LOGGER.log(Level.INFO, "Registering XML file {0}", file);
             try
-			{
+            {
                 //noinspection ObjectAllocationInLoop
                 inputStreams.add(new FileInputStream(file));
             } catch (FileNotFoundException ex)
@@ -100,23 +140,23 @@ public final class MessageFileFinder
             }
         }
         return inputStreams;
-	}
+    }
 
     private static Collection<InputStream> findMessageJarStreams() {
         Collection<InputStream> inputStreams = new LinkedList<>();
         Iterable<String> jarMessageEntries = findJarMessageEntries();
         for (String entry : jarMessageEntries)
-		{
-			LOGGER.log(Level.INFO, "Registering XML entry {0}", entry);
-			inputStreams.add(MessageFileFinder.class.getClassLoader().getResourceAsStream(entry));
-		}
-		return inputStreams;
-	}
+        {
+            LOGGER.log(Level.INFO, "Registering XML entry {0}", entry);
+            inputStreams.add(MessageFileFinder.class.getClassLoader().getResourceAsStream(entry));
+        }
+        return inputStreams;
+    }
 
     Collection<MessageGroup> findXmlRegistries() {
         Collection<InputStream> streamsToRead = new LinkedList<>();
         streamsToRead.addAll(findMessageJarStreams());
-		streamsToRead.addAll(findMessageFileStreams());
-		return MessageRegistryReader.readXmlStreams(streamsToRead);
-	}
+        streamsToRead.addAll(findMessageFileStreams());
+        return readXmlStreams(streamsToRead);
+    }
 }
