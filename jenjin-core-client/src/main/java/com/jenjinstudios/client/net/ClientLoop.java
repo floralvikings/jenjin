ff8c0d273da76278b3b37e7886e6f9b34f222240
Caleb Brinkman <floralvikings@gmail.com>
2015-02-14 00:37:54 -0600

    Removed unnecessary initializer

--- jenjin-core-client/src/main/java/com/jenjinstudios/client/net/ClientLoop.java
+++ jenjin-core-client/src/main/java/com/jenjinstudios/client/net/ClientLoop.java
@@ -11,51 +11,51 @@ import java.util.TimerTask;
 
 class ClientLoop extends TimerTask
 {
-	/** The Client for this loop. */
-	private final Client client;
-	private int updateCount = 0;
-	private long lastStart = System.nanoTime();
-	private final long[] updateTimesNanos = new long[50];
+    /** The Client for this loop. */
+    private final Client client;
+    private int updateCount;
+    private long lastStart = System.nanoTime();
+    private final long[] updateTimesNanos = new long[50];
 
-	/**
-	 * Construct a ClientLoop for the given client.
-	 *
-	 * @param client The client for this ClientLoop
-	 */
-	public ClientLoop(Client client) {
-		this.client = client;
-	}
+    /**
+     * Construct a ClientLoop for the given client.
+     *
+     * @param client The client for this ClientLoop
+     */
+    public ClientLoop(Client client) {
+        this.client = client;
+    }
 
-	@Override
-	public void run() {
-		updateCount++;
-		saveUpdateTime();
-		client.runRepeatedTasks();
-		client.getExecutableMessageQueue().runQueuedExecutableMessages();
-		try
-		{
-			client.getMessageIO().writeAllMessages();
-		} catch (IOException e)
-		{
-			client.shutdown();
-		}
-	}
+    @Override
+    public void run() {
+        updateCount++;
+        saveUpdateTime();
+        client.runRepeatedTasks();
+        client.getExecutableMessageQueue().runQueuedExecutableMessages();
+        try
+        {
+            client.getMessageIO().writeAllMessages();
+        } catch (IOException e)
+        {
+            client.shutdown();
+        }
+    }
 
-	private void saveUpdateTime() {
-		long newStart = System.nanoTime();
-		long timeElapsed = newStart - lastStart;
-		lastStart = newStart;
-		updateTimesNanos[updateCount % updateTimesNanos.length] = timeElapsed;
-	}
+    private void saveUpdateTime() {
+        long newStart = System.nanoTime();
+        long timeElapsed = newStart - lastStart;
+        lastStart = newStart;
+        updateTimesNanos[updateCount % updateTimesNanos.length] = timeElapsed;
+    }
 
-	public double getAverageRunTime() {
-		double maxIndex = Math.min(updateCount, updateTimesNanos.length);
-		double total = 0;
-		for (int i = 0; i < maxIndex; i++)
-		{
-			total += updateTimesNanos[i];
-		}
-		return (total / maxIndex) / 1000000000;
-	}
+    public double getAverageRunTime() {
+        double maxIndex = Math.min(updateCount, updateTimesNanos.length);
+        double total = 0;
+        for (int i = 0; i < maxIndex; i++)
+        {
+            total += updateTimesNanos[i];
+        }
+        return (total / maxIndex) / 1000000000;
+    }
 
 }
