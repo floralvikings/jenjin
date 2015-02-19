be15544bb9efb36e45ef80e001302d953d94ab38
Caleb Brinkman <floralvikings@gmail.com>
2015-02-19 00:23:24 -0600

    Code format according to intellij

--- jenjin-core-client/src/test/java/com/jenjinstudios/client/net/AuthClientTest.java
+++ jenjin-core-client/src/test/java/com/jenjinstudios/client/net/AuthClientTest.java
@@ -10,6 +10,7 @@ import static org.mockito.Mockito.mock;
 
 /**
  * Test the AuthClient class.
+ *
  * @author Caleb Brinkman
  */
 public class AuthClientTest
@@ -18,39 +19,39 @@ public class AuthClientTest
      * Test the isLoggedIn method.
      */
     @Test
-	public void testIsLoggedIn() {
-		MessageInputStream mis = mock(MessageInputStream.class);
-		MessageOutputStream mos = mock(MessageOutputStream.class);
-		MessageIO messageIO = new MessageIO(mis, mos);
-		ClientUser clientUser = mock(ClientUser.class);
+    public void testIsLoggedIn() {
+        MessageInputStream mis = mock(MessageInputStream.class);
+        MessageOutputStream mos = mock(MessageOutputStream.class);
+        MessageIO messageIO = new MessageIO(mis, mos);
+        ClientUser clientUser = mock(ClientUser.class);
         boolean random = ((Math.random() * 10) % 2) == 0;
         AuthClient authClient = new AuthClient(messageIO, clientUser);
-		authClient.getLoginTracker().setLoggedIn(random);
+        authClient.getLoginTracker().setLoggedIn(random);
 
-		Assert.assertEquals(authClient.getLoginTracker().isLoggedIn(), random);
-	}
+        Assert.assertEquals(authClient.getLoginTracker().isLoggedIn(), random);
+    }
 
-	@Test
-	public void testLoggedInTime() {
-		MessageInputStream mis = mock(MessageInputStream.class);
-		MessageOutputStream mos = mock(MessageOutputStream.class);
-		MessageIO messageIO = new MessageIO(mis, mos);
-		ClientUser clientUser = mock(ClientUser.class);
-		long random = (long) (Math.random() * 1000);
-		AuthClient authClient = new AuthClient(messageIO, clientUser);
-		authClient.getLoginTracker().setLoggedInTime(random);
+    @Test
+    public void testLoggedInTime() {
+        MessageInputStream mis = mock(MessageInputStream.class);
+        MessageOutputStream mos = mock(MessageOutputStream.class);
+        MessageIO messageIO = new MessageIO(mis, mos);
+        ClientUser clientUser = mock(ClientUser.class);
+        long random = (long) (Math.random() * 1000);
+        AuthClient authClient = new AuthClient(messageIO, clientUser);
+        authClient.getLoginTracker().setLoggedInTime(random);
 
-		Assert.assertEquals(authClient.getLoginTracker().getLoggedInTime(), random);
-	}
+        Assert.assertEquals(authClient.getLoginTracker().getLoggedInTime(), random);
+    }
 
-	@Test
-	public void testGetUser() {
-		MessageInputStream mis = mock(MessageInputStream.class);
-		MessageOutputStream mos = mock(MessageOutputStream.class);
-		MessageIO messageIO = new MessageIO(mis, mos);
-		ClientUser clientUser = mock(ClientUser.class);
-		AuthClient authClient = new AuthClient(messageIO, clientUser);
+    @Test
+    public void testGetUser() {
+        MessageInputStream mis = mock(MessageInputStream.class);
+        MessageOutputStream mos = mock(MessageOutputStream.class);
+        MessageIO messageIO = new MessageIO(mis, mos);
+        ClientUser clientUser = mock(ClientUser.class);
+        AuthClient authClient = new AuthClient(messageIO, clientUser);
 
-		Assert.assertEquals(authClient.getUser(), clientUser);
-	}
+        Assert.assertEquals(authClient.getUser(), clientUser);
+    }
 }
