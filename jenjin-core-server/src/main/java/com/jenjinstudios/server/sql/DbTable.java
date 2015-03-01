86f9b15745670eff239c3cef4e64e9b5c1513bb5
Caleb Brinkman <floralvikings@gmail.com>
2015-03-01 11:33:49 -0600

    Added docs for lookup method

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/sql/DbTable.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/sql/DbTable.java
@@ -7,5 +7,12 @@ package com.jenjinstudios.server.sql;
  */
 public interface DbTable<T>
 {
+	/**
+	 * Look up the object in the database with the given primary key.
+	 *
+	 * @param key The primary key used to make the lookup query.
+	 *
+	 * @return The object stored in the database with the given primary key.
+	 */
 	T lookup(String key);
 }
