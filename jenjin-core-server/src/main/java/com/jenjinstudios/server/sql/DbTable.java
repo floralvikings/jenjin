27d00500db6cde96312ffa7437ed0cda845790e8
Caleb Brinkman <floralvikings@gmail.com>
2015-03-01 11:32:51 -0600

    Adding some docs, removed unnecessary 'public' modifier

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/sql/DbTable.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/sql/DbTable.java
@@ -1,9 +1,11 @@
 package com.jenjinstudios.server.sql;
 
 /**
+ * Interface to be implemented by classes that will make database lookups.
+ *
  * @author Caleb Brinkman
  */
 public interface DbTable<T>
 {
-	public T lookup(String key);
+	T lookup(String key);
 }
