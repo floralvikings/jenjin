65a875bc6fab474021169ec7d486cab4e1004339
Caleb Brinkman <floralvikings@gmail.com>
2015-03-02 16:03:47 -0600

    Added modified lookup method to deal with multiple rows and collections.

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/database/DbTable.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/database/DbTable.java
@@ -1,5 +1,8 @@
 package com.jenjinstudios.server.database;
 
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Interface to be implemented by classes that will make database lookups.
  *
@@ -15,4 +18,16 @@ public interface DbTable<T>
 	 * @return The object stored in the database with the given primary key.
 	 */
 	T lookup(String key);
+
+	/**
+	 * Return the rows from the database with column names matching the keys, and records of those columns matching the
+	 * values.
+	 *
+	 * @param columnNamesAndValues A key-value collection wherein the keys are the names of the columns, and the values
+	 * are the values for which to return that row.
+	 *
+	 * @return A collection of retrieved objects.
+	 */
+	Collection<T> lookup(Map<String, Object> columnNamesAndValues);
+
 }
