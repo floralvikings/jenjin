4fafc3188bc80e40f90ea5d7b3f65dfa43a1f337
Caleb Brinkman <floralvikings@gmail.com>
2015-03-12 17:27:58 -0500

    Added player class

---
+++ jenjin-world-server/src/main/java/com/jenjinstudios/world/server/Player.java
@@ -0,0 +1,44 @@
+package com.jenjinstudios.world.server;
+
+import com.jenjinstudios.server.database.IUser;
+import com.jenjinstudios.world.Actor;
+
+/**
+ * Represents a Player in the game world.
+ *
+ * @author Caleb Brinkman
+ */
+public class Player extends Actor implements IUser
+{
+	private String password;
+	private String salt;
+	private boolean loggedIn;
+
+	public Player(String name) {
+		super(name);
+	}
+
+	@Override
+	public String getUsername() { return getName(); }
+
+	@Override
+	public void setUsername(String username) { setName(username); }
+
+	@Override
+	public String getPassword() { return password; }
+
+	@Override
+	public void setPassword(String password) { this.password = password; }
+
+	@Override
+	public String getSalt() { return salt; }
+
+	@Override
+	public void setSalt(String salt) { this.salt = salt; }
+
+	@Override
+	public boolean isLoggedIn() { return loggedIn; }
+
+	@Override
+	public void setLoggedIn(boolean loggedIn) { this.loggedIn = loggedIn; }
+}
