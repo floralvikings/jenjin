9790dbd26ffbded62dfda301c7db0d331b936fc9
Caleb Brinkman <floralvikings@gmail.com>
2015-03-12 00:50:22 -0500

    Updated docs

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/database/DbTable.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/database/DbTable.java
@@ -18,7 +18,7 @@ public interface DbTable<T>
 	 * @param where A key-value collection wherein the keys are the names of the columns, and the values are the values
 	 * for which to return that row.
 	 *
-	 * @return A collection of retrieved objects.
+	 * @return A collection of retrieved objects. This collection will be empty if no results were found.
 	 */
 	List<T> lookup(Map<String, Object> where);
 
@@ -29,7 +29,7 @@ public interface DbTable<T>
 	 * for which to update that row.
 	 * @param row The new data to place in the row.
 	 *
-	 * @return Whether the update was successful.
+	 * @return Whether the table was updated.
 	 */
 	boolean update(Map<String, Object> where, T row);
 }
