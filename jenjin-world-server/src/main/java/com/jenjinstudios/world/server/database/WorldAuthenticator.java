746076707840edb98926eb383088163c2d14793d
Caleb Brinkman <floralvikings@gmail.com>
2015-03-13 14:46:55 -0500

    Added second constructor, getting ready to completely remove the Authenticator subclasses

--- jenjin-world-server/src/main/java/com/jenjinstudios/world/server/database/WorldAuthenticator.java
+++ jenjin-world-server/src/main/java/com/jenjinstudios/world/server/database/WorldAuthenticator.java
@@ -1,6 +1,7 @@
 package com.jenjinstudios.world.server.database;
 
 import com.jenjinstudios.server.authentication.Authenticator;
+import com.jenjinstudios.server.authentication.UserLookup;
 import com.jenjinstudios.world.server.Player;
 import com.jenjinstudios.world.server.database.sql.PlayerTable;
 
@@ -8,6 +9,7 @@ import java.sql.Connection;
 
 /**
  * Handles SQL stuff for a WorldServer.
+ *
  * @author Caleb Brinkman
  */
 @SuppressWarnings("SpellCheckingInspection")
@@ -16,10 +18,20 @@ public class WorldAuthenticator extends Authenticator<Player>
 
 	/**
 	 * Create a new SQLHandler with the given database information, and connect to the database.
+	 *
 	 * @param connection The database connection used by the authenticator.
 	 */
 	public WorldAuthenticator(Connection connection) {
 		super(new PlayerTable(connection));
+	}
+
+	/**
+	 * Construct a new WorldAuthenticator using the supplied UserLookup.
+	 *
+	 * @param userLookup The UserLookup used to find and update users.
+	 */
+	public WorldAuthenticator(UserLookup<Player> userLookup) {
+		super(userLookup);
 	}
 
 }
