f1750d79656ed5c98a71e8c73daac90ccdfd37cb
Caleb Brinkman <floralvikings@gmail.com>
2015-03-13 14:53:36 -0500

    Fixed demo database

--- jenjin-demo/jenjin-demo-server/src/main/java/com/jenjinstudios/demo/server/Main.java
+++ jenjin-demo/jenjin-demo-server/src/main/java/com/jenjinstudios/demo/server/Main.java
@@ -54,29 +54,21 @@ public class Main
 			  "  `password` CHAR(64) NOT NULL," +
 			  "  `salt` CHAR(48) NOT NULL," +
 			  "  `loggedin` TINYINT NOT NULL DEFAULT '0'," +
+			  "  `xCoord` INT NOT NULL DEFAULT '0'," +
+			  "  `yCoord` INT NOT NULL DEFAULT '0'," +
+			  "  `zoneId` INT NOT NULL DEFAULT '0'," +
 			  "  PRIMARY KEY (username)" +
-			  ")");
-		statement.executeUpdate("CREATE TABLE jenjin_user_properties (" +
-			  " `username` VARCHAR(64) NOT NULL," +
-			  " `propertyName` VARCHAR(64) NOT NULL," +
-			  " `propertyValue` VARCHAR(64)," +
-			  " PRIMARY KEY (`username`, `propertyName`))");
+			  ')');
 		for (int i = 1; i < 100; i++)
 		{
 			statement.executeUpdate(
 				  "INSERT INTO jenjin_users " +
-						"(`username`, `password`, `salt`, `loggedin`)" +
+						"(`username`, `password`, `salt`, `loggedin`, `xCoord`, `yCoord`, `zoneId`)" +
 						" VALUES " +
 						"('TestAccount" + i + "', " +
 						"'650f00f552d4df0147d236e240ccfc490444f4b358c4ff1d79f5fd90f57243bd', " +
 						"'e3c42b85a183d3f654a3d2bb3bc5ea607d0fb529d9b890d3', " +
-						"'0')");
-			statement.executeUpdate("INSERT INTO jenjin_user_properties (`username`, `propertyName`, " +
-				  "`propertyValue`) " +
-				  "VALUES " +
-				  "('TestAccount" + i + "', 'xCoord', '0'), " +
-				  "('TestAccount" + i + "', 'yCoord', '0'), " +
-				  "('TestAccount" + i + "', 'zoneID', '0') ");
+						"'0', '0', '0')");
 		}
 		return testConnection;
 	}
