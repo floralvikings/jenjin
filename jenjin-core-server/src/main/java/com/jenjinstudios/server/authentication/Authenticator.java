58babab9ba108f9494d0c90c1c955a8fbe96d3ff
Caleb Brinkman <floralvikings@gmail.com>
2015-03-14 10:17:56 -0500

    Wrapping DbException in AuthenticationException again.  Think I'm going to wrap all the Authenticator exceptions in AuthenticationExceptions, since, y'know, that would make sense

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/authentication/Authenticator.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/authentication/Authenticator.java
@@ -66,7 +66,14 @@ public class Authenticator<T extends User>
 	 * @throws DbException If there is an exception when updating the database.
 	 */
 	public T logOutUser(String username) throws DbException {
-		T user = userLookup.findUser(username);
+		T user;
+		try
+		{
+			user = userLookup.findUser(username);
+		} catch (DbException ex)
+		{
+			throw new AuthenticationException("Exception when accessing authentication database", ex);
+		}
 		if ((user != null) && user.isLoggedIn())
 		{
 			user.setLoggedIn(false);
