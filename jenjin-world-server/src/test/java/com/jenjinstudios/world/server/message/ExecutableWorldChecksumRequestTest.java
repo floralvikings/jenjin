8d1826b24c887eafa2055d675474670f42ab1a2e
Caleb Brinkman <floralvikings@gmail.com>
2015-03-20 15:10:57 -0500

    Modified test to utilize power mock for static mocking

--- jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldChecksumRequestTest.java
+++ jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldChecksumRequestTest.java
@@ -5,6 +5,9 @@ import com.jenjinstudios.core.io.Message;
 import com.jenjinstudios.core.io.MessageRegistry;
 import com.jenjinstudios.world.server.WorldClientHandler;
 import com.jenjinstudios.world.server.WorldServer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.annotations.Test;
 
 import static org.mockito.Mockito.*;
@@ -12,19 +15,23 @@ import static org.mockito.Mockito.*;
 /**
  * @author Caleb Brinkman
  */
-public class ExecutableWorldChecksumRequestTest
+@PrepareForTest(WorldServerMessageFactory.class)
+public class ExecutableWorldChecksumRequestTest extends PowerMockTestCase
 {
-	@SuppressWarnings("unchecked")
 	@Test
 	public void testExecuteMessage() {
-		Message message = mock(Message.class);
 		Message response = MessageRegistry.getInstance().createMessage("WorldChecksumResponse");
 		byte[] checksum = {1, 2, 3, 4, 5};
 		response.setArgument("checksum", checksum);
+
+		// Mocks
+		PowerMockito.mockStatic(WorldServerMessageFactory.class);
 		WorldClientHandler clientHandler = mock(WorldClientHandler.class);
+		Message message = mock(Message.class);
 		WorldServer server = mock(WorldServer.class);
-		WorldServerMessageFactory messageFactory = mock(WorldServerMessageFactory.class);
 		MessageIO messageIO = mock(MessageIO.class);
+
+		// Mock returns
 		when(WorldServerMessageFactory.generateWorldChecksumResponse(any())).thenReturn(response);
 		when(server.getWorldFileChecksum()).thenReturn(checksum);
 		when(clientHandler.getServer()).thenReturn(server);
