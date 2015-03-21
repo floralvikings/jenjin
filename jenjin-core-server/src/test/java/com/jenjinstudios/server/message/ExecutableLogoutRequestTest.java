ef936f6cb9bcc34103656ef5453612c4c8609642
Caleb Brinkman <floralvikings@gmail.com>
2015-03-20 22:57:12 -0500

    Now using Server instead of TaskedServer

--- jenjin-core-server/src/test/java/com/jenjinstudios/server/message/ExecutableLogoutRequestTest.java
+++ jenjin-core-server/src/test/java/com/jenjinstudios/server/message/ExecutableLogoutRequestTest.java
@@ -6,7 +6,7 @@ import com.jenjinstudios.server.authentication.Authenticator;
 import com.jenjinstudios.server.authentication.BasicUser;
 import com.jenjinstudios.server.authentication.User;
 import com.jenjinstudios.server.net.ClientHandler;
-import com.jenjinstudios.server.net.TaskedServer;
+import com.jenjinstudios.server.net.Server;
 import org.mockito.Mockito;
 import org.testng.annotations.Test;
 
@@ -27,7 +27,7 @@ public class ExecutableLogoutRequestTest
 		user.setUsername("foo");
 		user.setUsername("bar");
 		ClientHandler clientHandler = mock(ClientHandler.class);
-		TaskedServer server = mock(TaskedServer.class);
+		Server server = mock(Server.class);
 		Authenticator<BasicUser> authenticator = mock(Authenticator.class);
 		when(server.getAuthenticator()).thenReturn(authenticator);
 		when(server.getCycleStartTime()).thenReturn(12345l);
