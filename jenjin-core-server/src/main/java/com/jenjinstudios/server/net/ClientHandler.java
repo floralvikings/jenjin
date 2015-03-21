6e2f4f73df3c137a2ce6637d0f18e2637856c3bb
Caleb Brinkman <floralvikings@gmail.com>
2015-03-20 22:56:25 -0500

    Now using Server instead of TaskedServer

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/net/ClientHandler.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/net/ClientHandler.java
@@ -19,7 +19,7 @@ public class ClientHandler extends Connection
 {
     private static final Logger LOGGER = Logger.getLogger(ClientHandler.class.getName());
     /** The server. */
-	private final TaskedServer server;
+	private final Server server;
 	/** The id of the client handler. */
 	private int handlerId = -1;
     /** The time at which this client was successfully logged in. */
@@ -33,7 +33,7 @@ public class ClientHandler extends Connection
      * @param s The server for which this handler works.
      * @param messageIO The MessageIO used to send and receive messages.
      */
-	public ClientHandler(TaskedServer s, MessageIO messageIO) {
+	public ClientHandler(Server s, MessageIO messageIO) {
 		super(messageIO);
         setName("ClientHandler with unset ID");
         server = s;
@@ -73,7 +73,7 @@ public class ClientHandler extends Connection
      *
      * @return The server for which this client handler works.
      */
-	public TaskedServer getServer() { return server; }
+	public Server getServer() { return server; }
 
     /**
      * Queue a message indicating the success or failure of a logout attempt.
