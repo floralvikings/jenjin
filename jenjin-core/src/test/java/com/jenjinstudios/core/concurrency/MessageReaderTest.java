aac062df3868b35a0361f64ba4d168ebcdce5176
Caleb Brinkman <floralvikings@gmail.com>
2015-03-22 19:30:36 -0500

    Added MessageReaderTest

---
+++ jenjin-core/src/test/java/com/jenjinstudios/core/concurrency/MessageReaderTest.java
@@ -0,0 +1,88 @@
+package com.jenjinstudios.core.concurrency;
+
+import com.jenjinstudios.core.io.Message;
+import com.jenjinstudios.core.io.MessageInputStream;
+import com.jenjinstudios.core.io.MessageRegistry;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Deque;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Test the MessageReader class.
+ *
+ * @author Caleb Brinkman
+ */
+public class MessageReaderTest
+{
+	/**
+	 * Test the asynchronous reading and storage of incoming messages.
+	 *
+	 * @throws Exception If there's and exception.
+	 */
+	@Test
+	public void testAsynchronousReading() throws Exception {
+		Message message = MessageRegistry.getInstance().createMessage("PingRequest");
+		message.setArgument("requestTimeMillis", 12345L);
+		MessageInputStream inputStream = mock(MessageInputStream.class);
+		// Mock a blocking read.
+		when(inputStream.readMessage()).thenReturn(message).then(invocationOnMock -> {
+			Thread.sleep(10000);
+			return null;
+		});
+		MessageReader reader = new MessageReader(inputStream);
+		reader.start();
+
+		// Wait a little bit to make sure the timer can run at least once.
+		Thread.sleep(100);
+
+		Deque<Message> receivedMessages = reader.getReceivedMessages();
+		Assert.assertEquals(receivedMessages.size(), 1, "Should have one received message");
+
+		reader.stop();
+	}
+
+	/**
+	 * Test the isErrored method.
+	 *
+	 * @throws Exception If there's an exception.
+	 */
+	@Test
+	public void testIsErrored() throws Exception {
+		MessageInputStream inputStream = mock(MessageInputStream.class);
+		// Mock a blocking read.
+		when(inputStream.readMessage()).then(invocationOnMock -> {
+			Thread.sleep(10000);
+			return null;
+		});
+		MessageReader reader = new MessageReader(inputStream);
+		reader.start();
+		Assert.assertFalse(reader.isErrored(), "Reader should not error.");
+		reader.stop();
+	}
+
+	/**
+	 * Test the isErrored method with an error caused.
+	 *
+	 * @throws Exception If there's an exception.
+	 */
+	@Test
+	public void testIsErroredWithError() throws Exception {
+		MessageInputStream inputStream = mock(MessageInputStream.class);
+		// Mock a blocking read.
+		when(inputStream.readMessage()).thenThrow(new IOException("Error")).then(invocationOnMock -> {
+			Thread.sleep(10000);
+			return null;
+		});
+		MessageReader reader = new MessageReader(inputStream);
+		reader.start();
+		// Give the reader time to encounter the error.
+		Thread.sleep(100);
+		Assert.assertTrue(reader.isErrored(), "Reader should have error.");
+		reader.stop();
+	}
+}
