5a797302f793c04f353aa9ead69f9ea48f7efc03
Caleb Brinkman <floralvikings@gmail.com>
2015-03-23 06:28:14 -0500

    Modified test to use mocks instead of relying on MessageRegistry.

--- jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableWorldFileResponseTest.java
+++ jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableWorldFileResponseTest.java
@@ -1,7 +1,6 @@
 package com.jenjinstudios.world.client.message;
 
 import com.jenjinstudios.core.io.Message;
-import com.jenjinstudios.core.io.MessageRegistry;
 import com.jenjinstudios.world.client.ServerWorldFileTracker;
 import com.jenjinstudios.world.client.WorldClient;
 import org.testng.annotations.Test;
@@ -19,9 +18,8 @@ public class ExecutableWorldFileResponseTest
 	@Test
 	public void testMessageExecution() {
 		byte[] fileBytes = {1, 2, 3, 4, 5};
-		MessageRegistry messageRegistry = MessageRegistry.getInstance();
-		Message message = messageRegistry.createMessage("WorldFileResponse");
-		message.setArgument("fileBytes", fileBytes);
+		Message message = mock(Message.class);
+		when(message.getArgument("fileBytes")).thenReturn(fileBytes);
 
 		WorldClient worldClient = mock(WorldClient.class);
 		ServerWorldFileTracker serverWorldFileTracker = new ServerWorldFileTracker(worldClient, null);
