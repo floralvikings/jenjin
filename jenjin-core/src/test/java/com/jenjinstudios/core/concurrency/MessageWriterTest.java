4b1d1f7e74a683fdddef9f6474916b682bee9222
Caleb Brinkman <floralvikings@gmail.com>
2015-03-22 19:45:16 -0500

    Added MessageWriterTest

---
+++ jenjin-core/src/test/java/com/jenjinstudios/core/concurrency/MessageWriterTest.java
@@ -0,0 +1,78 @@
+package com.jenjinstudios.core.concurrency;
+
+import com.jenjinstudios.core.io.Message;
+import com.jenjinstudios.core.io.MessageOutputStream;
+import org.mockito.Mockito;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test the MessageWriter class.
+ *
+ * @author Caleb Brinkman
+ */
+public class MessageWriterTest
+{
+	/**
+	 * Test the asynchronous writing functionality of the MessageWriter class.
+	 *
+	 * @throws Exception If there's an exception.
+	 */
+	@Test
+	public void testAsynchronousWrite() throws Exception {
+		Message message = mock(Message.class);
+		MessageOutputStream outputStream = mock(MessageOutputStream.class);
+
+		MessageWriter writer = new MessageWriter(outputStream);
+		writer.enqueue(message);
+		writer.start();
+		// Give the writer time to write the message.
+		Thread.sleep(100);
+		verify(outputStream).writeMessage(message);
+		writer.stop();
+	}
+
+	/**
+	 * Test the isErrored function without an error being thrown.
+	 *
+	 * @throws Exception If there's an exception.
+	 */
+	@Test
+	public void testIsErrored() throws Exception {
+		Message message = mock(Message.class);
+		MessageOutputStream outputStream = mock(MessageOutputStream.class);
+
+		MessageWriter writer = new MessageWriter(outputStream);
+		writer.enqueue(message);
+		writer.start();
+		// Give the writer time to write the message.
+		Thread.sleep(100);
+		Assert.assertFalse(writer.isErrored(), "Writer should not have errored.");
+		writer.stop();
+	}
+
+	/**
+	 * Test the isErrored function with an error being thrown.
+	 *
+	 * @throws Exception If there's an exception.
+	 */
+	@Test
+	public void testIsErroredWithError() throws Exception {
+		Message message = mock(Message.class);
+		MessageOutputStream outputStream = mock(MessageOutputStream.class);
+		Mockito.doThrow(new IOException("Error")).when(outputStream).writeMessage(message);
+
+		MessageWriter writer = new MessageWriter(outputStream);
+		writer.enqueue(message);
+		writer.start();
+		// Give the writer time to error.
+		Thread.sleep(100);
+		Assert.assertTrue(writer.isErrored(), "Writer should have errored.");
+		writer.stop();
+	}
+}
