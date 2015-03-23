f2141db6835c6babe814ffa90035742819794da9
Caleb Brinkman <floralvikings@gmail.com>
2015-03-23 06:26:19 -0500

    Modified test to use mocks instead of relying on MessageRegistry.

--- jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableActorMoveSpeedMessageTest.java
+++ jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableActorMoveSpeedMessageTest.java
@@ -1,12 +1,13 @@
 package com.jenjinstudios.world.client.message;
 
 import com.jenjinstudios.core.io.Message;
-import com.jenjinstudios.core.io.MessageRegistry;
 import com.jenjinstudios.world.Actor;
 import com.jenjinstudios.world.client.WorldClient;
 import org.mockito.Mockito;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import static org.mockito.Mockito.when;
 
 /**
  * @author Caleb Brinkman
@@ -15,13 +16,12 @@ public class ExecutableActorMoveSpeedMessageTest
 {
 	@Test
 	public void testMessageExecution() throws Exception {
-		MessageRegistry messageRegistry = MessageRegistry.getInstance();
-		Message actorMoveSpeedMessage = messageRegistry.createMessage("ActorMoveSpeed");
-		actorMoveSpeedMessage.setArgument("moveSpeed", 123.45);
+		Message msg = Mockito.mock(Message.class);
+		when(msg.getArgument("moveSpeed")).thenReturn(123.45);
 		WorldClient worldClient = Mockito.mock(WorldClient.class);
 
 		ExecutableActorMoveSpeedMessage message =
-			  new ExecutableActorMoveSpeedMessage(worldClient, actorMoveSpeedMessage);
+			  new ExecutableActorMoveSpeedMessage(worldClient, msg);
 		message.runImmediate();
 		message.runDelayed();
 
