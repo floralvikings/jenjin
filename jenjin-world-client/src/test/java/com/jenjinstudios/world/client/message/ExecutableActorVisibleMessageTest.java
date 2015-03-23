b6e834c627f59770d2cb840f99694b357ff8fb5f
Caleb Brinkman <floralvikings@gmail.com>
2015-03-23 06:21:13 -0500

    Modified test to use mocks instead of relying on MessageRegistry.

--- jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableActorVisibleMessageTest.java
+++ jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableActorVisibleMessageTest.java
@@ -1,7 +1,6 @@
 package com.jenjinstudios.world.client.message;
 
 import com.jenjinstudios.core.io.Message;
-import com.jenjinstudios.core.io.MessageRegistry;
 import com.jenjinstudios.world.World;
 import com.jenjinstudios.world.client.WorldClient;
 import com.jenjinstudios.world.collections.WorldObjectList;
@@ -17,17 +16,16 @@ public class ExecutableActorVisibleMessageTest
 {
 	@Test
 	public void testMessageExecution() throws Exception {
-		MessageRegistry messageRegistry = MessageRegistry.getInstance();
-		Message actorVisibleMessage = messageRegistry.createMessage("ActorVisibleMessage");
-		actorVisibleMessage.setArgument("name", "a1b2c3d4e5f6890");
-		actorVisibleMessage.setArgument("id", 100);
-		actorVisibleMessage.setArgument("resourceID", 100);
-		actorVisibleMessage.setArgument("xCoordinate", 1.0);
-		actorVisibleMessage.setArgument("yCoordinate", 1.0);
-		actorVisibleMessage.setArgument("relativeAngle", Angle.IDLE);
-		actorVisibleMessage.setArgument("absoluteAngle", 0.0);
-		actorVisibleMessage.setArgument("timeOfVisibility", 100l);
-		actorVisibleMessage.setArgument("moveSpeed", 10.0d);
+		Message actorVisibleMessage = mock(Message.class);
+		when(actorVisibleMessage.getArgument("name")).thenReturn("a1b2c3d4e5f6890");
+		when(actorVisibleMessage.getArgument("id")).thenReturn(100);
+		when(actorVisibleMessage.getArgument("resourceID")).thenReturn(100);
+		when(actorVisibleMessage.getArgument("xCoordinate")).thenReturn(1.0);
+		when(actorVisibleMessage.getArgument("yCoordinate")).thenReturn(1.0);
+		when(actorVisibleMessage.getArgument("relativeAngle")).thenReturn(Angle.IDLE);
+		when(actorVisibleMessage.getArgument("absoluteAngle")).thenReturn(0.0);
+		when(actorVisibleMessage.getArgument("timeOfVisibility")).thenReturn(100L);
+		when(actorVisibleMessage.getArgument("moveSpeed")).thenReturn(10.0d);
 
 		WorldClient worldClient = mock(WorldClient.class);
 		World world = mock(World.class);
