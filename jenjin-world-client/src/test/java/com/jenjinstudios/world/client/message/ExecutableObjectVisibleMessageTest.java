8cbfa521c4ce1cb3ae62a037f8b14d310095eb16
Caleb Brinkman <floralvikings@gmail.com>
2015-03-23 06:27:25 -0500

    Modified test to use mocks instead of relying on MessageRegistry.

--- jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableObjectVisibleMessageTest.java
+++ jenjin-world-client/src/test/java/com/jenjinstudios/world/client/message/ExecutableObjectVisibleMessageTest.java
@@ -1,7 +1,6 @@
 package com.jenjinstudios.world.client.message;
 
 import com.jenjinstudios.core.io.Message;
-import com.jenjinstudios.core.io.MessageRegistry;
 import com.jenjinstudios.world.World;
 import com.jenjinstudios.world.client.WorldClient;
 import com.jenjinstudios.world.collections.WorldObjectList;
@@ -16,13 +15,12 @@ public class ExecutableObjectVisibleMessageTest
 {
 	@Test
 	public void testMessageExecution() throws Exception {
-		MessageRegistry messageRegistry = MessageRegistry.getInstance();
-		Message actorVisibleMessage = messageRegistry.createMessage("ObjectVisibleMessage");
-		actorVisibleMessage.setArgument("name", "a1b2c3d4e5f6890");
-		actorVisibleMessage.setArgument("id", 100);
-		actorVisibleMessage.setArgument("resourceID", 100);
-		actorVisibleMessage.setArgument("xCoordinate", 1.0);
-		actorVisibleMessage.setArgument("yCoordinate", 1.0);
+		Message actorVisibleMessage = mock(Message.class);
+		when(actorVisibleMessage.getArgument("name")).thenReturn("a1b2c3d4e5f6890");
+		when(actorVisibleMessage.getArgument("id")).thenReturn(100);
+		when(actorVisibleMessage.getArgument("resourceID")).thenReturn(100);
+		when(actorVisibleMessage.getArgument("xCoordinate")).thenReturn(1.0);
+		when(actorVisibleMessage.getArgument("yCoordinate")).thenReturn(1.0);
 
 		WorldClient worldClient = mock(WorldClient.class);
 		World world = mock(World.class);
