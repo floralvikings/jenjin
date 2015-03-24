6f5ba2c40a2a28ecc58b2fd4156f9720b5e875b4
Caleb Brinkman <floralvikings@gmail.com>
2015-03-24 18:26:51 -0500

    Added MessageRegistryTest

---
+++ jenjin-core/src/test/java/com/jenjinstudios/core/io/MessageRegistryTest.java
@@ -0,0 +1,64 @@
+package com.jenjinstudios.core.io;
+
+import com.jenjinstudios.core.xml.MessageType;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.InputStream;
+
+/**
+ * Test the MessageRegistry class.
+ *
+ * @author Caleb Brinkman
+ */
+public class MessageRegistryTest
+{
+	/**
+	 * Test the register method.
+	 */
+	@Test
+	public void testRegister() {
+		String messageFile = "test/jenjinstudios/core/Messages.xml";
+		InputStream in = getClass().getClassLoader().getResourceAsStream(messageFile);
+		String name = "Test Message Group";
+
+		MessageRegistry messageRegistry = new MessageRegistry();
+		messageRegistry.register(name, in);
+
+		MessageType message = messageRegistry.getMessageType("BlankMessage");
+		Assert.assertNotNull(message, "Message type should have been registered");
+	}
+
+	/**
+	 * Test the clear method.
+	 */
+	@Test
+	public void testClear() {
+		String messageFile = "test/jenjinstudios/core/Messages.xml";
+		InputStream in = getClass().getClassLoader().getResourceAsStream(messageFile);
+		String name = "Test Message Group";
+
+		MessageRegistry messageRegistry = new MessageRegistry();
+		messageRegistry.register(name, in);
+		messageRegistry.clear();
+
+		MessageType message = messageRegistry.getMessageType("BlankMessage");
+		Assert.assertNull(message, "Message type should have been cleared");
+	}
+
+	/**
+	 * Test the createMessage method.
+	 */
+	@Test
+	public void testCreateMessage() {
+		String messageFile = "test/jenjinstudios/core/Messages.xml";
+		InputStream in = getClass().getClassLoader().getResourceAsStream(messageFile);
+		String name = "Test Message Group";
+
+		MessageRegistry messageRegistry = new MessageRegistry();
+		messageRegistry.register(name, in);
+
+		Message message = messageRegistry.createMessage("BlankMessage");
+		Assert.assertEquals(message.getID(), -1, "Message type should have ID 0");
+	}
+}
