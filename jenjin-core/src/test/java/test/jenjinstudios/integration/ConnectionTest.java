7243c194b16e27e249922ac908f36fd531a9a25a
Caleb Brinkman <floralvikings@gmail.com>
2015-03-24 22:15:52 -0500

    Working on primitive integration test

---
+++ jenjin-core/src/test/java/test/jenjinstudios/integration/ConnectionTest.java
@@ -0,0 +1,69 @@
+package test.jenjinstudios.integration;
+
+import com.jenjinstudios.core.Connection;
+import com.jenjinstudios.core.MessageIO;
+import com.jenjinstudios.core.io.Message;
+import com.jenjinstudios.core.io.MessageInputStream;
+import com.jenjinstudios.core.io.MessageOutputStream;
+import com.jenjinstudios.core.io.MessageRegistry;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.KeyPair;
+
+/**
+ * Fully test the Connection class, by creating two sockets for IO and testing various functionality.
+ *
+ * @author Caleb Brinkman
+ */
+public class ConnectionTest
+{
+	/**
+	 * Run the integration tests for Connection.
+	 *
+	 * @throws Exception If there's an exception.
+	 */
+	@Test
+	public void integrationTest() throws Exception {
+		InputStream stream = getClass().
+			  getClassLoader().
+			  getResourceAsStream("test/jenjinstudios/core/Messages.xml");
+		MessageRegistry.getGlobalRegistry().register("Integration test messages", stream);
+
+		ServerSocket serverSocket = new ServerSocket(51015);
+		Socket socketOne = new Socket("localhost", 51015);
+		Socket socketTwo = serverSocket.accept();
+
+		MessageInputStream inputStreamOne = new MessageInputStream(socketOne.getInputStream());
+		MessageOutputStream outputStreamOne = new MessageOutputStream(socketTwo.getOutputStream());
+		MessageIO messageIOOne = new MessageIO(inputStreamOne, outputStreamOne);
+
+		MessageInputStream inputStreamTwo = new MessageInputStream(socketTwo.getInputStream());
+		MessageOutputStream outputStreamTwo = new MessageOutputStream(socketTwo.getOutputStream());
+		MessageIO messageIOTwo = new MessageIO(inputStreamTwo, outputStreamTwo);
+
+		Connection connectionOne = new Connection(messageIOOne);
+		Connection connectionTwo = new Connection(messageIOTwo);
+
+		connectionOne.start();
+		connectionTwo.start();
+
+		KeyPair keyPairOne = Connection.generateRSAKeyPair();
+		KeyPair keyPairTwo = Connection.generateRSAKeyPair();
+
+		connectionOne.setRSAKeyPair(keyPairOne);
+		connectionTwo.setRSAKeyPair(keyPairTwo);
+
+		Message message = MessageRegistry.getGlobalRegistry().createMessage("Test");
+		message.setArgument("encryptedString", "FooBar");
+
+		connectionOne.enqueueMessage(message);
+
+		// Give the second connection time to read the message
+		Thread.sleep(100);
+		Assert.assertEquals(connectionTwo.getName(), "FooBar", "Connection name should be set by executable message.");
+	}
+}
---
+++ jenjin-core/src/test/java/test/jenjinstudios/integration/message/ExecutableTest.java
@@ -0,0 +1,33 @@
+package test.jenjinstudios.integration.message;
+
+import com.jenjinstudios.core.Connection;
+import com.jenjinstudios.core.ExecutableMessage;
+import com.jenjinstudios.core.io.Message;
+
+/**
+ * @author Caleb Brinkman
+ */
+public class ExecutableTest extends ExecutableMessage
+{
+	/**
+	 * Construct a new ExecutableMessage; this should only ever be invoked reflectively, by a {@code Connection}'s
+	 * update cycle.
+	 *
+	 * @param connection The connection for which this ExecutbleMessage will work.
+	 * @param message The message that caused this {@code ExecutableMessage} to be created.
+	 */
+	@SuppressWarnings("UnusedDeclaration")
+	protected ExecutableTest(Connection connection, Message message) {
+		super(connection, message);
+	}
+
+	@Override
+	public void runDelayed() {
+
+	}
+
+	@Override
+	public void runImmediate() {
+		getConnection().setName((String) getMessage().getArgument("encryptedString"));
+	}
+}
