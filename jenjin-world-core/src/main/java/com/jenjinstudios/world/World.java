e23a20cfd4abea58c58c1e92eb2e44f33adf8f90
Caleb Brinkman <floralvikings@gmail.com>
2015-03-25 23:01:45 -0500

    Working on removing runDelayed method

--- jenjin-world-core/src/main/java/com/jenjinstudios/world/World.java
+++ jenjin-world-core/src/main/java/com/jenjinstudios/world/World.java
@@ -2,8 +2,10 @@ package com.jenjinstudios.world;
 
 import com.jenjinstudios.world.collections.WorldObjectList;
 
+import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
  * Contains all the Zones, Locations and GameObjects.
@@ -16,6 +18,7 @@ public class World
 	private final TreeMap<Integer, Zone> zones = new TreeMap<>();
 	/** The GameObjects contained in the world. */
 	private final transient WorldObjectList worldObjects = new WorldObjectList(this);
+	private final transient Collection<Runnable> scheduledUpdateTasks = new ConcurrentLinkedQueue<>();
 	/** The time at which the most recent update completed. */
 	private transient long lastUpdateCompleted;
 	/** The start time of the most recent update. */
@@ -46,11 +49,24 @@ public class World
 			worldObjects.forEach(WorldObject::preUpdate);
 			worldObjects.forEach(WorldObject::update);
 			worldObjects.forEach(WorldObject::postUpdate);
+			scheduledUpdateTasks.forEach(Runnable::run);
 		}
 		lastUpdateCompleted = System.currentTimeMillis();
 	}
 
 	public Map<Integer, Zone> getZones() { return zones; }
+
+	/**
+	 * Schedule a task to be executed after the next update.
+	 *
+	 * @param task The task to execute.
+	 */
+	public void scheduleUpdateTask(Runnable task) {
+		synchronized (scheduledUpdateTasks)
+		{
+			scheduledUpdateTasks.add(task);
+		}
+	}
 
 	public long getLastUpdateCompleted() { return lastUpdateCompleted; }
 
