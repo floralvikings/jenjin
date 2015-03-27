a66049470388cec714eefac3a77155c8beab65e5
Caleb Brinkman <floralvikings@gmail.com>
2015-03-27 17:13:00 -0500

    Removed queueExecutableMessage method

--- jenjin-core/src/main/java/com/jenjinstudios/core/ExecutableMessageQueue.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/ExecutableMessageQueue.java
@@ -12,16 +12,4 @@ public class ExecutableMessageQueue
 {
     private final Collection<ExecutableMessage> queuedExecutableMessages = new LinkedList<>();
 
-    /**
-     * Add an {@code ExecutableMessage} to the end of the queue.
-     *
-     * @param executableMessage The {@code ExecutableMessage} to add.
-     */
-    protected void queueExecutableMessage(ExecutableMessage executableMessage) {
-        synchronized (queuedExecutableMessages)
-        {
-            queuedExecutableMessages.add(executableMessage);
-        }
-    }
-
 }
