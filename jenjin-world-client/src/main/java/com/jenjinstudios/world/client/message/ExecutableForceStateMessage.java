59da42312485e7aa1b7354f0ad04dde6ff1d16bb
Caleb Brinkman <floralvikings@gmail.com>
2015-03-27 00:33:04 -0500

    Moved logic to runDelayed method using World.ScheduleUpdateTask

--- jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableForceStateMessage.java
+++ jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableForceStateMessage.java
@@ -2,6 +2,7 @@ package com.jenjinstudios.world.client.message;
 
 import com.jenjinstudios.core.io.Message;
 import com.jenjinstudios.world.Actor;
+import com.jenjinstudios.world.World;
 import com.jenjinstudios.world.client.WorldClient;
 import com.jenjinstudios.world.math.Angle;
 import com.jenjinstudios.world.math.Vector2D;
@@ -14,21 +15,16 @@ import com.jenjinstudios.world.util.ActorUtils;
  */
 public class ExecutableForceStateMessage extends WorldClientExecutableMessage
 {
-    private Vector2D vector2D;
-    private Angle angle;
+	private static final float MS_TO_S = 1000.0F;
+	private Vector2D vector2D;
+	private Angle angle;
     private long timeOfForce;
 
     public ExecutableForceStateMessage(WorldClient client, Message message) { super(client, message); }
 
     @Override
     public void runDelayed() {
-        Actor player = getConnection().getPlayer();
-        double dist = ((player.getWorld().getLastUpdateCompleted() - timeOfForce) / 1000) * player.getMoveSpeed();
-        Vector2D corrected = vector2D.getVectorInDirection(dist, angle.getStepAngle());
-        player.setVector2D(corrected);
-        player.setAngle(angle);
 
-        ActorUtils.forceIdle(player);
     }
 
     @Override
@@ -40,5 +36,16 @@ public class ExecutableForceStateMessage extends WorldClientExecutableMessage
         timeOfForce = (long) getMessage().getArgument("timeOfForce");
         angle = new Angle(absoluteAngle, relativeAngle);
         vector2D = new Vector2D(x, y);
-    }
+
+		World world = getConnection().getWorld();
+		world.scheduleUpdateTask(() -> {
+			Actor player = getConnection().getPlayer();
+			double dist = ((world.getLastUpdateCompleted() - timeOfForce) / MS_TO_S) * player.getMoveSpeed();
+			Vector2D corrected = vector2D.getVectorInDirection(dist, angle.getStepAngle());
+			player.setVector2D(corrected);
+			player.setAngle(angle);
+
+			ActorUtils.forceIdle(player);
+		});
+	}
 }
