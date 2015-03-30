012f0628d23441c6e5d52b62473704da1f2a9385
Caleb Brinkman <floralvikings@gmail.com>
2015-03-30 17:45:07 -0500

    Removed soon-to-be-defunct test

--- jenjin-core-server/src/test/java/com/jenjinstudios/server/net/ClientHandlerTest.java
+++ jenjin-core-server/src/test/java/com/jenjinstudios/server/net/ClientHandlerTest.java
@@ -1,13 +1,11 @@
 package com.jenjinstudios.server.net;
 
-import com.jenjinstudios.core.io.Message;
 import com.jenjinstudios.core.io.MessageIO;
 import com.jenjinstudios.core.io.MessageInputStream;
 import com.jenjinstudios.core.io.MessageOutputStream;
 import com.jenjinstudios.server.authentication.Authenticator;
 import com.jenjinstudios.server.authentication.BasicUser;
 import com.jenjinstudios.server.message.ServerMessageFactory;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.testng.PowerMockTestCase;
 import org.testng.annotations.Test;
@@ -22,24 +20,6 @@ import static org.testng.Assert.assertEquals;
 @PrepareForTest(ServerMessageFactory.class)
 public class ClientHandlerTest extends PowerMockTestCase
 {
-
-	@Test
-	public void testSendLogoutStatus() throws Exception {
-		PowerMockito.mockStatic(ServerMessageFactory.class);
-		Message message = mock(Message.class);
-		when(ServerMessageFactory.generateLogoutResponse(true)).thenReturn(message);
-		Server server = mock(Server.class);
-		MessageInputStream mis = mock(MessageInputStream.class);
-		MessageOutputStream mos = mock(MessageOutputStream.class);
-		MessageIO messageIO = new MessageIO(mis, mos);
-
-		ClientHandler clientHandler = new ClientHandler(server, messageIO);
-		clientHandler.sendLogoutStatus(true);
-		clientHandler.start();
-		Thread.sleep(20);
-		verify(mos, times(1)).writeMessage(any());
-		clientHandler.shutdown();
-	}
 
 	@Test
 	public void testSetID() throws Exception {
