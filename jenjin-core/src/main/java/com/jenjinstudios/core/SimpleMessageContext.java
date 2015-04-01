d5627c35dd3e7dfcd9d3b0bf5a3f71fa89103923
Caleb Brinkman <floralvikings@gmail.com>
2015-04-01 02:44:38 -0500

    Added PingTracker to SimpleMessageContext

--- jenjin-core/src/main/java/com/jenjinstudios/core/SimpleMessageContext.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/SimpleMessageContext.java
@@ -10,14 +10,25 @@ import com.jenjinstudios.core.concurrency.MessageContext;
 public class SimpleMessageContext implements MessageContext
 {
 	private final String name;
+	private final PingTracker pingTracker;
 
 	/**
 	 * Construct a new MessageContext with the given name.
 	 *
 	 * @param name The name of the context.
 	 */
-	public SimpleMessageContext(String name) { this.name = name; }
+	public SimpleMessageContext(String name) {
+		this.name = name;
+		this.pingTracker = new PingTracker();
+	}
 
 	@Override
 	public String getName() { return name; }
+
+	/**
+	 * Get the PingTracker associated with this MessageContext.
+	 *
+	 * @return The PingTracker associated with this MessageContext.
+	 */
+	public PingTracker getPingTracker() { return pingTracker; }
 }
