bb34dd34766ca8966d13c4b5ed75bf1189e15689
Caleb Brinkman <floralvikings@gmail.com>
2015-04-01 23:29:04 -0500

    Added getVerifiedKeys method

--- jenjin-core/src/main/java/com/jenjinstudios/core/concurrency/MessageContext.java
+++ jenjin-core/src/main/java/com/jenjinstudios/core/concurrency/MessageContext.java
@@ -1,6 +1,8 @@
 package com.jenjinstudios.core.concurrency;
 
+import java.net.InetAddress;
 import java.security.Key;
+import java.util.Map;
 
 /**
  * Used to represent data that should be passed into an executable message on construction.
@@ -29,4 +31,12 @@ public interface MessageContext
 	 * @param encryptionKey The key used to encrypt messages sent in this context.
 	 */
 	void setEncryptionKey(Key encryptionKey);
+
+	/**
+	 * Get the map of internet addresses and keys with which they are associated that are verified to be correlated.
+	 *
+	 * @return The map of internet addresses and keys with which they are associated that are verified to be
+	 * correlated.
+	 */
+	Map<InetAddress, Key> getVerifiedKeys();
 }
