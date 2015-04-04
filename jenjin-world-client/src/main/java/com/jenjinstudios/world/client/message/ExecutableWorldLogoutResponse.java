1bb558c0c537ddc8b292df290bfd360099bd625d
Caleb Brinkman <floralvikings@gmail.com>
2015-04-04 17:42:26 -0500

    Proper type generics

--- jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableWorldLogoutResponse.java
+++ jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableWorldLogoutResponse.java
@@ -1,15 +1,15 @@
 package com.jenjinstudios.world.client.message;
 
-import com.jenjinstudios.client.net.ClientMessageContext;
 import com.jenjinstudios.core.io.Message;
 import com.jenjinstudios.world.client.WorldClient;
+import com.jenjinstudios.world.client.WorldClientMessageContext;
 
 /**
  * Handles logout responses.
  *
  * @author Caleb Brinkman
  */
-public class ExecutableWorldLogoutResponse extends WorldClientExecutableMessage<ClientMessageContext>
+public class ExecutableWorldLogoutResponse extends WorldClientExecutableMessage<WorldClientMessageContext>
 {
     /**
      * Construct an ExecutableMessage with the given Message.
@@ -18,7 +18,7 @@ public class ExecutableWorldLogoutResponse extends WorldClientExecutableMessage<
      * @param message The Message.
 	 * @param context The context in which to execute the message.
 	 */
-	public ExecutableWorldLogoutResponse(WorldClient client, Message message, ClientMessageContext context) {
+	public ExecutableWorldLogoutResponse(WorldClient client, Message message, WorldClientMessageContext context) {
 		super(client, message, context);
 	}
 
