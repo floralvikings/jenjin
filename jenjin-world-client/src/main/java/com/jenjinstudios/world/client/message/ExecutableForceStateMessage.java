d6aca818f4a8de44023d1c6da0df81b7b04a0964
Caleb Brinkman <floralvikings@gmail.com>
2015-04-04 03:15:49 -0500

    Making use of context instead of client

--- jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableForceStateMessage.java
+++ jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableForceStateMessage.java
@@ -1,10 +1,10 @@
 package com.jenjinstudios.world.client.message;
 
-import com.jenjinstudios.client.net.ClientMessageContext;
 import com.jenjinstudios.core.io.Message;
 import com.jenjinstudios.world.Actor;
 import com.jenjinstudios.world.World;
 import com.jenjinstudios.world.client.WorldClient;
+import com.jenjinstudios.world.client.WorldClientMessageContext;
 import com.jenjinstudios.world.math.Angle;
 import com.jenjinstudios.world.math.Vector2D;
 import com.jenjinstudios.world.util.ActorUtils;
@@ -14,16 +14,16 @@ import com.jenjinstudios.world.util.ActorUtils;
  *
  * @author Caleb Brinkman
  */
-public class ExecutableForceStateMessage extends WorldClientExecutableMessage<ClientMessageContext>
+public class ExecutableForceStateMessage extends WorldClientExecutableMessage<WorldClientMessageContext>
 {
 	private static final float MS_TO_S = 1000.0F;
 	private Vector2D vector2D;
 	private Angle angle;
     private long timeOfForce;
 
-	public ExecutableForceStateMessage(WorldClient client, Message message, ClientMessageContext context) {
-		super(client,
-		  message, context); }
+	public ExecutableForceStateMessage(WorldClient client, Message message, WorldClientMessageContext context) {
+		super(client, message, context);
+	}
 
     @Override
 	public Message execute() {
@@ -35,9 +35,9 @@ public class ExecutableForceStateMessage extends WorldClientExecutableMessage<Cl
         angle = new Angle(absoluteAngle, relativeAngle);
         vector2D = new Vector2D(x, y);
 
-		World world = getWorldClient().getWorld();
+		World world = getContext().getWorld();
 		world.scheduleUpdateTask(() -> {
-			Actor player = getWorldClient().getPlayer();
+			Actor player = getContext().getPlayer();
 			double dist = ((world.getLastUpdateCompleted() - timeOfForce) / MS_TO_S) * player.getMoveSpeed();
 			Vector2D corrected = vector2D.getVectorInDirection(dist, angle.getStepAngle());
 			player.setVector2D(corrected);
