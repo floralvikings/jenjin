bb94958b6168185ac7c5becd1e406120a886c851
Caleb Brinkman <floralvikings@gmail.com>
2015-04-04 03:18:49 -0500

    Proper type generics

--- jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableWorldChecksumResponse.java
+++ jenjin-world-client/src/main/java/com/jenjinstudios/world/client/message/ExecutableWorldChecksumResponse.java
@@ -1,15 +1,15 @@
 package com.jenjinstudios.world.client.message;
 
-import com.jenjinstudios.client.net.ClientMessageContext;
 import com.jenjinstudios.core.io.Message;
 import com.jenjinstudios.world.client.WorldClient;
+import com.jenjinstudios.world.client.WorldClientMessageContext;
 
 /**
  * Handles login responses from the server.
  *
  * @author Caleb Brinkman
  */
-public class ExecutableWorldChecksumResponse extends WorldClientExecutableMessage<ClientMessageContext>
+public class ExecutableWorldChecksumResponse extends WorldClientExecutableMessage<WorldClientMessageContext>
 {
     /**
      * Construct an ExecutableMessage with the given Message.
@@ -18,7 +18,7 @@ public class ExecutableWorldChecksumResponse extends WorldClientExecutableMessag
      * @param message The Message.
 	 * @param context The context in which to execute the message.
 	 */
-	public ExecutableWorldChecksumResponse(WorldClient client, Message message, ClientMessageContext context) {
+	public ExecutableWorldChecksumResponse(WorldClient client, Message message, WorldClientMessageContext context) {
 		super(client, message, context);
 	}
 
