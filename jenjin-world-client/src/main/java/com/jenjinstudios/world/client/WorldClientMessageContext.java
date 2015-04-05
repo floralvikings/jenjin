9667172a65172c416cc5814106587cfb99a2cc3c
Caleb Brinkman <floralvikings@gmail.com>
2015-04-05 03:44:15 -0500

    Added worldFileTracker property

--- jenjin-world-client/src/main/java/com/jenjinstudios/world/client/WorldClientMessageContext.java
+++ jenjin-world-client/src/main/java/com/jenjinstudios/world/client/WorldClientMessageContext.java
@@ -11,6 +11,7 @@ import com.jenjinstudios.world.World;
  */
 public class WorldClientMessageContext extends ClientMessageContext
 {
+	private final WorldFileTracker worldFileTracker = new WorldFileTracker();
 	private Actor player;
 	private World world;
 
@@ -41,4 +42,11 @@ public class WorldClientMessageContext extends ClientMessageContext
 	 * @param world The world to be managed by this context.
 	 */
 	public void setWorld(World world) { this.world = world; }
+
+	/**
+	 * Get the WorldFileTracker managed by this context.
+	 *
+	 * @return The WorldFileTracker managed by this context.
+	 */
+	public WorldFileTracker getWorldFileTracker() { return worldFileTracker; }
 }
