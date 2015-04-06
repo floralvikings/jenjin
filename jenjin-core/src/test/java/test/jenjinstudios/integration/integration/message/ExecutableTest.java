a8d0aa6ccc7e263fb172aad59c3b402966870d17
Caleb Brinkman <floralvikings@gmail.com>
2015-04-06 16:28:28 -0500

    Removed deprecated parameter

--- jenjin-core/src/test/java/test/jenjinstudios/integration/integration/message/ExecutableTest.java
+++ jenjin-core/src/test/java/test/jenjinstudios/integration/integration/message/ExecutableTest.java
@@ -1,6 +1,5 @@
 package test.jenjinstudios.integration.integration.message;
 
-import com.jenjinstudios.core.Connection;
 import com.jenjinstudios.core.concurrency.ExecutableMessage;
 import com.jenjinstudios.core.concurrency.MessageContext;
 import com.jenjinstudios.core.io.Message;
@@ -15,12 +14,11 @@ public class ExecutableTest extends ExecutableMessage<MessageContext>
 	 * Construct a new ExecutableMessage; this should only ever be invoked reflectively, by a {@code Connection}'s
 	 * update cycle.
 	 *
-	 * @param connection The connection for which this ExecutbleMessage will work.
 	 * @param message The message that caused this {@code ExecutableMessage} to be created.
 	 * @param context The context in which to execute the message.
 	 */
 	@SuppressWarnings("UnusedDeclaration")
-	public ExecutableTest(Connection connection, Message message, MessageContext context) {
+	public ExecutableTest(Message message, MessageContext context) {
 		super(message, context);
 	}
 
