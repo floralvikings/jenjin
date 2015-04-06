4a9670242172a2cb632fe3f9840c1ea469075fe0
Caleb Brinkman <floralvikings@gmail.com>
2015-04-06 01:18:05 -0500

    Using correct type parameter

--- jenjin-world-server/src/main/java/com/jenjinstudios/world/server/message/ExecutableWorldChecksumRequest.java
+++ jenjin-world-server/src/main/java/com/jenjinstudios/world/server/message/ExecutableWorldChecksumRequest.java
@@ -1,16 +1,17 @@
 package com.jenjinstudios.world.server.message;
 
 import com.jenjinstudios.core.io.Message;
-import com.jenjinstudios.server.net.ServerMessageContext;
+import com.jenjinstudios.world.server.Player;
 import com.jenjinstudios.world.server.WorldClientHandler;
 import com.jenjinstudios.world.server.WorldServer;
+import com.jenjinstudios.world.server.WorldServerMessageContext;
 
 /**
  * Process a WorldChecksumRequest.
  *
  * @author Caleb Brinkman
  */
-public class ExecutableWorldChecksumRequest extends WorldExecutableMessage<ServerMessageContext>
+public class ExecutableWorldChecksumRequest extends WorldExecutableMessage<WorldServerMessageContext<Player>>
 {
 	/**
 	 * Construct a new ExecutableMessage.  Must be implemented by subclasses.
@@ -19,7 +20,9 @@ public class ExecutableWorldChecksumRequest extends WorldExecutableMessage<Serve
 	 * @param message The message.
 	 * @param context The context in which to execute the message.
 	 */
-	public ExecutableWorldChecksumRequest(WorldClientHandler handler, Message message, ServerMessageContext context) {
+	public ExecutableWorldChecksumRequest(WorldClientHandler handler, Message message,
+										  WorldServerMessageContext<Player> context)
+	{
 		super(handler, message, context);
 	}
 
