f737c57b8e6fe67f5b732ba87f0894f4ea1fecdf
Caleb Brinkman <floralvikings@gmail.com>
2015-04-06 00:46:16 -0500

    Update test for context

--- jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableStateChangeRequestTest.java
+++ jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableStateChangeRequestTest.java
@@ -87,19 +87,22 @@ public class ExecutableStateChangeRequestTest
 		Player player = new Player("FooBar");
 		world.getWorldObjects().add(player);
 		world.update();
+
 		WorldClientHandler mock = mock(WorldClientHandler.class);
-		when(mock.getUser()).thenReturn(player);
 		WorldServer worldServer = mock(WorldServer.class);
+		Message request = mock(Message.class);
+		ServerMessageContext<Player> context = mock(ServerMessageContext.class);
+
+		when(context.getUser()).thenReturn(player);
 		when(mock.getServer()).thenReturn(worldServer);
 		when(worldServer.getUps()).thenReturn(50);
-		Message request = mock(Message.class);
 		when(request.getArgument("relativeAngle")).thenReturn(Angle.FRONT);
 		when(request.getArgument("absoluteAngle")).thenReturn(0.0);
 		when(request.getArgument("xCoordinate")).thenReturn(0.0);
 		when(request.getArgument("yCoordinate")).thenReturn(0.0);
 		when(request.getArgument("timeOfChange")).thenReturn(System.currentTimeMillis() - 2000);
-		ExecutableStateChangeRequest executableStateChangeRequest = new ExecutableStateChangeRequest(mock, request, null);
-		executableStateChangeRequest.execute();
+		ExecutableStateChangeRequest exec = new ExecutableStateChangeRequest(mock, request, context);
+		exec.execute();
 		world.update();
 
 		Assert.assertEquals(player.getAngle(), new Angle(0.0, Angle.IDLE));
