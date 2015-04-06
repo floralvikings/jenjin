c43f77e03403a280266422833d445cac9724a175
Caleb Brinkman <floralvikings@gmail.com>
2015-04-06 14:59:05 -0500

    Removed unused parameter

--- jenjin-core/src/test/java/com/jenjinstudios/core/message/ExecutableBlankMessage.java
+++ jenjin-core/src/test/java/com/jenjinstudios/core/message/ExecutableBlankMessage.java
@@ -1,6 +1,5 @@
 package com.jenjinstudios.core.message;
 
-import com.jenjinstudios.core.Connection;
 import com.jenjinstudios.core.concurrency.ExecutableMessage;
 import com.jenjinstudios.core.concurrency.MessageContext;
 import com.jenjinstudios.core.io.Message;
@@ -19,13 +18,11 @@ public class ExecutableBlankMessage extends ExecutableMessage<MessageContext>
 
     /**
      * Construct a new ExecutableBlankMessage; not meant to be used in production code.
-     *
-     * @param connection The connection invoking this message.
-     * @param message The message causing this to be invoked.
+	 *  @param message The message causing this to be invoked.
 	 * @param context The context in which to execute the message.
 	 */
     @SuppressWarnings("WeakerAccess")
-	public ExecutableBlankMessage(Connection connection, Message message, MessageContext context) {
+	public ExecutableBlankMessage(Message message, MessageContext context) {
 		super(message, context);
 	}
 
