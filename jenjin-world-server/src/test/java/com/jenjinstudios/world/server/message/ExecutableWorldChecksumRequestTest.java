33c4e2bf3c10e6c91a3cbf5a4eb49d1b4676e8ff
Caleb Brinkman <floralvikings@gmail.com>
2015-04-08 19:33:57 -0500

    Removed unused mocks

--- jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldChecksumRequestTest.java
+++ jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldChecksumRequestTest.java
@@ -1,9 +1,6 @@
 package com.jenjinstudios.world.server.message;
 
 import com.jenjinstudios.core.io.Message;
-import com.jenjinstudios.core.io.MessageStreamPair;
-import com.jenjinstudios.world.server.WorldClientHandler;
-import com.jenjinstudios.world.server.WorldServer;
 import com.jenjinstudios.world.server.WorldServerMessageContext;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -32,17 +29,12 @@ public class ExecutableWorldChecksumRequestTest extends PowerMockTestCase
 
 		// Mocks
 		PowerMockito.mockStatic(WorldServerMessageFactory.class);
-		WorldClientHandler clientHandler = mock(WorldClientHandler.class);
 		Message message = mock(Message.class);
-		WorldServer server = mock(WorldServer.class);
 		WorldServerMessageContext context = mock(WorldServerMessageContext.class);
-		MessageStreamPair messageStreamPair = mock(MessageStreamPair.class);
 
 		// Mock returns
 		when(WorldServerMessageFactory.generateWorldChecksumResponse(any())).thenReturn(response);
 		when(context.getWorldChecksum()).thenReturn(checksum);
-		when(clientHandler.getServer()).thenReturn(server);
-		when(clientHandler.getMessageStreamPair()).thenReturn(messageStreamPair);
 
 		ExecutableWorldChecksumRequest exec = new ExecutableWorldChecksumRequest(message, context);
 		Message resp = exec.execute();
