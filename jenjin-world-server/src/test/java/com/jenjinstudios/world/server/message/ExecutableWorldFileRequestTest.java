6b2a54d78ab8244ea5d06df214cc05314836c79c
Caleb Brinkman <floralvikings@gmail.com>
2015-04-08 19:35:11 -0500

    Removed unused mocks

--- jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldFileRequestTest.java
+++ jenjin-world-server/src/test/java/com/jenjinstudios/world/server/message/ExecutableWorldFileRequestTest.java
@@ -1,9 +1,6 @@
 package com.jenjinstudios.world.server.message;
 
 import com.jenjinstudios.core.io.Message;
-import com.jenjinstudios.core.io.MessageStreamPair;
-import com.jenjinstudios.world.server.WorldClientHandler;
-import com.jenjinstudios.world.server.WorldServer;
 import com.jenjinstudios.world.server.WorldServerMessageContext;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -33,15 +30,10 @@ public class ExecutableWorldFileRequestTest extends PowerMockTestCase
 		when(response.getArgument("fileBytes")).thenReturn(fileBytes);
 
 		PowerMockito.mockStatic(WorldServerMessageFactory.class);
-		WorldClientHandler clientHandler = mock(WorldClientHandler.class);
-		WorldServer server = mock(WorldServer.class);
-		MessageStreamPair messageStreamPair = mock(MessageStreamPair.class);
 		WorldServerMessageContext context = mock(WorldServerMessageContext.class);
 
 		when(WorldServerMessageFactory.generateWorldFileResponse(any())).thenReturn(response);
 		when(context.getWorldBytes()).thenReturn(fileBytes);
-		when(clientHandler.getServer()).thenReturn(server);
-		when(clientHandler.getMessageStreamPair()).thenReturn(messageStreamPair);
 
 		ExecutableWorldFileRequest exec = new ExecutableWorldFileRequest(message, context);
 		Message resp = exec.execute();
