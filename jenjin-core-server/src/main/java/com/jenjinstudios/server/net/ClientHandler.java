3e9d42863633c41b2fae194400cb45bf353ff138
Caleb Brinkman <floralvikings@gmail.com>
2015-04-14 15:41:51 -0500

    Removed out of date, lying, docs

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/net/ClientHandler.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/net/ClientHandler.java
@@ -7,27 +7,15 @@ import com.jenjinstudios.server.authentication.AuthenticationException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- * The {@code ClientHandler} class is used to communicate with an individual client.
- *
- * @author Caleb Brinkman
- */
 public class ClientHandler<T extends ServerMessageContext> extends Connection<T>
 {
     private static final Logger LOGGER = Logger.getLogger(ClientHandler.class.getName());
 
-	/**
-	 * Construct a new Client Handler using the given socket.  When constructing a new ClientHandler, it is necessary to
-     * send the client a FirstConnectResponse message with the server's UPS
-	 * @param messageStreamPair The MessageIO used to send and receive messages.
-	 * @param context The context in which messages should be executed.
-	 */
 	public ClientHandler(MessageStreamPair messageStreamPair, T context) {
 		super(messageStreamPair, context);
 		getMessageContext().setName("ClientHandler " + getId());
 	}
 
-	/** Shut down the client handler. */
 	@Override
     public void shutdown() {
 		if (getMessageContext().getUser() != null)
