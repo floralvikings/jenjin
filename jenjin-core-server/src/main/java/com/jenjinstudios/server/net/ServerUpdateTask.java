2cb748b7937fb1a134baf4827f8f25085c97d1db
Caleb Brinkman <floralvikings@gmail.com>
2015-04-20 17:59:04 -0500

    Removed everything; class is just a placeholder so compilation doesn't fail.  Tests and demos working as expected

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/net/ServerUpdateTask.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/net/ServerUpdateTask.java
@@ -1,39 +1,7 @@
 package com.jenjinstudios.server.net;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class ServerUpdateTask implements Runnable
 {
-	private static final Logger LOGGER = Logger.getLogger(ServerUpdateTask.class.getName());
-	private final List<Runnable> repeatedTasks;
-
-	/**
-	 * Construct a new {@code ServerLoop} for the specified server.
-	 */
-	@SuppressWarnings("unchecked")
-	public ServerUpdateTask() {
-		repeatedTasks = new LinkedList<>();
-	}
-
 	@Override
-	public void run() {
-		runRepeatedTasks();
-	}
-
-	private void runRepeatedTasks() {
-		try
-		{
-			synchronized (repeatedTasks)
-			{
-				repeatedTasks.forEach(Runnable::run);
-			}
-		} catch (RuntimeException ex)
-		{
-			LOGGER.log(Level.WARNING, "Exception when running repeated tasks.", ex);
-		}
-	}
-
+	public void run() { }
 }
