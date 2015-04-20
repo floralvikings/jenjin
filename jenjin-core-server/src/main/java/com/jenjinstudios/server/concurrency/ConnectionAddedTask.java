48ceacd8dc3137dce3c3eda0f8d0c528d9f083fd
Caleb Brinkman <floralvikings@gmail.com>
2015-04-20 00:59:42 -0500

    Generified ConnectionAddedTask

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/concurrency/ConnectionAddedTask.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/concurrency/ConnectionAddedTask.java
@@ -1,6 +1,6 @@
 package com.jenjinstudios.server.concurrency;
 
-import com.jenjinstudios.core.Connection;
+import com.jenjinstudios.core.concurrency.MessageContext;
 
 /**
  * Invoked when a Connection is added to a connection pool.
@@ -8,12 +8,12 @@ import com.jenjinstudios.core.Connection;
  * @author Caleb Brinkman
  */
 @FunctionalInterface
-public interface ConnectionAddedTask
+public interface ConnectionAddedTask<T extends MessageContext>
 {
 	/**
 	 * Called when a connection is added to the ConnectionPool.
 	 *
 	 * @param connection The connection that has been added.
 	 */
-	void connectionAdded(Connection connection);
+	void connectionAdded(T connection);
 }
