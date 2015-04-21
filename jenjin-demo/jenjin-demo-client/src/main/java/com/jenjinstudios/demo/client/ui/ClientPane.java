4810aca3799db4ec26ad96918782dd817b1fff3e
Caleb Brinkman <floralvikings@gmail.com>
2015-04-21 01:33:37 -0500

    Made variable generic for casting purposes

--- jenjin-demo/jenjin-demo-client/src/main/java/com/jenjinstudios/demo/client/ui/ClientPane.java
+++ jenjin-demo/jenjin-demo-client/src/main/java/com/jenjinstudios/demo/client/ui/ClientPane.java
@@ -4,6 +4,7 @@ import com.jenjinstudios.world.client.WorldClient;
 import javafx.animation.AnimationTimer;
 import javafx.event.EventHandler;
 import javafx.geometry.Dimension2D;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -13,7 +14,7 @@ public class ClientPane extends GridPane
 {
 	public ClientPane(WorldClient worldClient, Dimension2D size) {
 		PlayerViewCanvas canvas = new PlayerViewCanvas(worldClient, size.getWidth(), size.getHeight());
-		EventHandler playerControlKeyHandler = new PlayerControlKeyHandler(worldClient);
+		EventHandler<KeyEvent> playerControlKeyHandler = new PlayerControlKeyHandler(worldClient);
 		canvas.setOnKeyPressed(playerControlKeyHandler);
 		canvas.setOnKeyReleased(playerControlKeyHandler);
 		add(canvas, 0, 0);
