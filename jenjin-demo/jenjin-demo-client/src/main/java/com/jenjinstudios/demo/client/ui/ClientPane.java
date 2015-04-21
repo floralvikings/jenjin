79a2ed31a6ca3c7c0c2e2330c800a3e39c4a0013
Caleb Brinkman <floralvikings@gmail.com>
2015-04-21 01:15:44 -0500

    Weakened type

--- jenjin-demo/jenjin-demo-client/src/main/java/com/jenjinstudios/demo/client/ui/ClientPane.java
+++ jenjin-demo/jenjin-demo-client/src/main/java/com/jenjinstudios/demo/client/ui/ClientPane.java
@@ -2,6 +2,7 @@ package com.jenjinstudios.demo.client.ui;
 
 import com.jenjinstudios.world.client.WorldClient;
 import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
 import javafx.geometry.Dimension2D;
 import javafx.scene.layout.GridPane;
 
@@ -12,7 +13,7 @@ public class ClientPane extends GridPane
 {
 	public ClientPane(WorldClient worldClient, Dimension2D size) {
 		PlayerViewCanvas canvas = new PlayerViewCanvas(worldClient, size.getWidth(), size.getHeight());
-		PlayerControlKeyHandler playerControlKeyHandler = new PlayerControlKeyHandler(worldClient);
+		EventHandler playerControlKeyHandler = new PlayerControlKeyHandler(worldClient);
 		canvas.setOnKeyPressed(playerControlKeyHandler);
 		canvas.setOnKeyReleased(playerControlKeyHandler);
 		add(canvas, 0, 0);
