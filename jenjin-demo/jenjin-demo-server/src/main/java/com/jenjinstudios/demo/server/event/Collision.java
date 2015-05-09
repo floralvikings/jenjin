c8309ddc65d46632941f0a545203aeb39b1c122a
Caleb Brinkman <floralvikings@gmail.com>
2015-05-09 12:47:09 -0500

    Cleaning up analysis warnings

--- jenjin-demo/jenjin-demo-server/src/main/java/com/jenjinstudios/demo/server/event/Collision.java
+++ jenjin-demo/jenjin-demo-server/src/main/java/com/jenjinstudios/demo/server/event/Collision.java
@@ -6,19 +6,28 @@ import com.jenjinstudios.world.event.PostUpdateEvent;
 import com.jenjinstudios.world.math.Vector2D;
 import javafx.geometry.Rectangle2D;
 
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * PostUpdateEvent class which determines if a WorldObject is colliding with another WorldObject.
+ *
  * @author Caleb Brinkman
  */
 public abstract class Collision implements PostUpdateEvent
 {
+	/** The name of the property in the WorldObject that indicates size. */
 	public static final String SIZE_PROPERTY = "ObjectSize";
-	public static final double DEFAULT_OBJECT_SIZE = Location.SIZE;
+	private static final double DEFAULT_OBJECT_SIZE = Location.SIZE;
 	private static final Logger LOGGER = Logger.getLogger(Collision.class.getName());
 	private final WorldObject worldObject;
 
+	/**
+	 * Consctruct a new Collision which will check for collisions with the specified WorldObject.
+	 *
+	 * @param worldObject The WorldObject which will be checked for collisions.
+	 */
 	public Collision(WorldObject worldObject) { this.worldObject = worldObject; }
 
 	@Override
@@ -28,8 +37,7 @@ public abstract class Collision implements PostUpdateEvent
 
 	private boolean checkForCollision(WorldObject collider) {
 		boolean collision = false;
-		if (checkRoughCollision(collider))
-		{
+		if (checkRoughCollision(collider)) {
 			collision = checkFinerCollision(collider);
 		}
 		return collision;
@@ -37,7 +45,7 @@ public abstract class Collision implements PostUpdateEvent
 
 	private boolean checkRoughCollision(WorldObject collider) {
 		// TODO Make this more granular.
-		return collider != worldObject && collider.getZoneID() == worldObject.getZoneID();
+		return !Objects.equals(collider, worldObject) && (collider.getZoneID() == worldObject.getZoneID());
 	}
 
 	private boolean checkFinerCollision(WorldObject collider) {
@@ -47,7 +55,7 @@ public abstract class Collision implements PostUpdateEvent
 		return objectRect.intersects(colliderRect);
 	}
 
-	private Rectangle2D getCollisionRect(WorldObject obj) {
+	private static Rectangle2D getCollisionRect(WorldObject obj) {
 		double objectRad = calculateCollisionRad(obj);
 		Vector2D objectVector = obj.getVector2D();
 		double objectX = objectVector.getXCoordinate();
@@ -57,17 +65,22 @@ public abstract class Collision implements PostUpdateEvent
 		return new Rectangle2D(objectMinX, objectMinY, objectRad, objectRad);
 	}
 
-	protected double calculateCollisionRad(WorldObject collider) {
+	private static double calculateCollisionRad(WorldObject collider) {
 		Object o = collider.getProperties().get(SIZE_PROPERTY);
-		try
-		{
-			return (o != null ? (double) o : DEFAULT_OBJECT_SIZE) / 2;
-		} catch (ClassCastException ex)
-		{
-			LOGGER.log(Level.WARNING, "Object size not instance of double: {0}, {1}", new Object[]{collider, o});
-			return DEFAULT_OBJECT_SIZE / 2;
+		double rad;
+		try {
+			rad = ((o != null) ? (double) o : DEFAULT_OBJECT_SIZE) / 2;
+		} catch (ClassCastException ex) {
+			LOGGER.log(Level.WARNING, "Object size not instance of double: " + collider + ", " + o, ex);
+			rad = DEFAULT_OBJECT_SIZE / 2;
 		}
+		return rad;
 	}
 
+	/**
+	 * This method should be overridden in subclasses to specify what even should occur when a collision is triggered.
+	 *
+	 * @param collided The object with wich the WorldObject watched by this Collision has collided.
+	 */
 	public abstract void onCollision(WorldObject collided);
 }
