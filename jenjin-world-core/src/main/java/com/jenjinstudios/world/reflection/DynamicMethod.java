5d34855919dfce6a1faa036a0c71ff2be03a23f9
Caleb Brinkman <floralvikings@gmail.com>
2015-06-05 18:50:11 -0500

    Format brackets better

--- jenjin-world-core/src/main/java/com/jenjinstudios/world/reflection/DynamicMethod.java
+++ jenjin-world-core/src/main/java/com/jenjinstudios/world/reflection/DynamicMethod.java
@@ -13,6 +13,4 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-public @interface DynamicMethod
-{
-}
+public @interface DynamicMethod {}
