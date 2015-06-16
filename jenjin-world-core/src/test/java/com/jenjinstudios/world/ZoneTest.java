95e5040d5e7f47b99b648b4ea068e5de8eefef2b
Caleb Brinkman <floralvikings@gmail.com>
2015-06-15 22:11:23 -0500

    Add testGetAdjacentCells method

--- jenjin-world-core/src/test/java/com/jenjinstudios/world/ZoneTest.java
+++ jenjin-world-core/src/test/java/com/jenjinstudios/world/ZoneTest.java
@@ -4,6 +4,8 @@ import com.jenjinstudios.world.math.Dimensions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.Collection;
+
 /**
  * Test the Zone class; specifically, test that it initializes properly.
  *
@@ -26,5 +28,29 @@ public class ZoneTest
 
 		zone.getCell((short) 10, (short) 12, (short) 10);
 		Assert.assertEquals(zone.populatedCellCount(), 45, "18 further cells should have been populated.");
+	}
+
+	/**
+	 * Test the getAdjacentCells method.
+	 */
+	@Test
+	public void testGetAdjacentCells() {
+		Zone zone = new Zone(new Dimensions(100, 100, 100));
+
+		Cell center = zone.getCell(49, 49, 49);
+
+		Collection<Cell> adjacents = zone.getAdjacentCells(center);
+
+		Assert.assertEquals(adjacents.size(), 26, "Should be 3 cells cubed, minus center");
+
+		for (Cell cell : adjacents) {
+			int xDiff = Math.abs(center.getPoint().getXCoordinate() - cell.getPoint().getXCoordinate());
+			int yDiff = Math.abs(center.getPoint().getYCoordinate() - cell.getPoint().getYCoordinate());
+			int zDiff = Math.abs(center.getPoint().getZCoordinate() - cell.getPoint().getZCoordinate());
+
+			Assert.assertTrue(xDiff <= 1, "X difference must be less than or equal to one");
+			Assert.assertTrue(yDiff <= 1, "Y difference must be less than or equal to one");
+			Assert.assertTrue(zDiff <= 1, "Z difference must be less than or equal to one");
+		}
 	}
 }
