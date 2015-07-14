c9f864591c906b226af1fa1f312e103af0ac6c3c
Caleb Brinkman <floralvikings@gmail.com>
2015-07-13 23:56:29 -0500

    Added task fields and getters

--- jenjin-core-server/src/main/java/com/jenjinstudios/server/net/ServerConfig.java
+++ jenjin-core-server/src/main/java/com/jenjinstudios/server/net/ServerConfig.java
@@ -1,8 +1,14 @@
 package com.jenjinstudios.server.net;
 
 import com.jenjinstudios.core.connection.ConnectionConfig;
+import com.jenjinstudios.server.concurrency.ConnectionAddedTask;
+import com.jenjinstudios.server.concurrency.ShutdownTask;
+import com.jenjinstudios.server.concurrency.UpdateTask;
 
 import java.security.KeyPair;
+import java.util.Collection;
+
+import static java.util.Collections.unmodifiableCollection;
 
 /**
  * Used for configuring a Server.
@@ -11,6 +17,9 @@ import java.security.KeyPair;
  */
 public class ServerConfig<T extends ServerMessageContext> extends ConnectionConfig<T>
 {
+	private Collection<UpdateTask> updateTasks;
+	private Collection<ConnectionAddedTask> connectionAddedTasks;
+	private Collection<ShutdownTask> shutdownTasks;
 	private KeyPair keyPair;
 	private int ups;
 
@@ -27,4 +36,27 @@ public class ServerConfig<T extends ServerMessageContext> extends ConnectionConf
 	 * @return The updates per second at which the server should update.
 	 */
 	public int getUps() { return ups; }
+
+	/**
+	 * Get the update tasks to be run by the server.
+	 *
+	 * @return The update tasks to be run by the server.
+	 */
+	public Collection<UpdateTask> getUpdateTasks() { return unmodifiableCollection(updateTasks); }
+
+	/**
+	 * Get the connection added tasks to be run by the server.
+	 *
+	 * @return The connection added tasks to be run by the server.
+	 */
+	public Collection<ConnectionAddedTask> getConnectionAddedTasks() {
+		return unmodifiableCollection(connectionAddedTasks);
+	}
+
+	/**
+	 * Get the shutdown tasks to be run by the server.
+	 *
+	 * @return The shutdown tasks to be run by the server.
+	 */
+	public Collection<ShutdownTask> getShutdownTasks() { return unmodifiableCollection(shutdownTasks); }
 }
